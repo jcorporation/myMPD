1f815889736530fb57a1e2513a652511e922aed0
jcorporation <mail@jcgames.de>
2018-06-21 22:56:39 +0100

    Add minified files

--- mkrelease.sh
+++ mkrelease.sh
@@ -2,17 +2,21 @@
 
 if [ -f buildtools/closure-compiler.jar ]
 then
-  rm -f htdocs/js/player.min.js
-  java -jar buildtools/closure-compiler.jar htdocs/js/player.js > htdocs/js/player.min.js
-
-  rm -f htdocs/js/mpd.min.js
-  java -jar buildtools/closure-compiler.jar htdocs/js/mpd.js > htdocs/js/mpd.min.js
+  [ htdocs/js/player.js -nt htdocs/js/player.min.js ] && \
+    java -jar buildtools/closure-compiler.jar htdocs/js/player.js > htdocs/js/player.min.js
+  [ htdocs/js/mpd.js -nt  htdocs/js/mpd.min.js ] && \
+    java -jar buildtools/closure-compiler.jar htdocs/js/mpd.js > htdocs/js/mpd.min.js
+else
+  cp htdocs/js/player.js htdocs/js/player.min.js
+  cp htdocs/js/mpd.js  htdocs/js/mpd.min.js
 fi
   
 if [ -f buildtools/closure-stylesheets.jar ]
 then    
-  rm -f htdocs/css/mpd.min.css
-  java -jar buildtools/closure-stylesheets.jar htdocs/css/mpd.css > htdocs/css/mpd.min.css
+  [ htdocs/css/mpd.css -nt htdocs/css/mpd.min.css ] && \
+    java -jar buildtools/closure-stylesheets.jar htdocs/css/mpd.css > htdocs/css/mpd.min.css
+else
+  cp htdocs/css/mpd.css htdocs/css/mpd.min.css    
 fi
 
 [ -d release ] || mkdir release
@@ -20,3 +24,5 @@ cd release
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=RELEASE ..
 make
 make install
+sed -i -e 's/mpd\.css/mpd\.min\.css/' -e 's/mpd\.js/mpd\.min\.js/' /usr/share/mympd/htdocs/index.html
+sed -i -e 's/mpd\.css/mpd\.min\.css/' -e 's/player\.js/player\.min\.js/' /usr/share/mympd/htdocs/player.html
