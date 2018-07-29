ef3d8be28bd07a4446e50fda8b44ddae3508b83e
jcorporation <mail@jcgames.de>
2018-07-29 23:07:43 +0100

    fix: pull request #25, put $LIBRARY in quotation marks

--- mkrelease.sh
+++ mkrelease.sh
@@ -54,7 +54,7 @@ echo "Trying to link musicdir to library"
 if [ -f /etc/mpd.conf ]
 then
   LIBRARY=$(grep music /etc/mpd.conf | awk {'print $2'} | sed -e 's/"//g')
-  [ "$LIBRARY" != "" ] && [ ! -e /usr/share/mympd/htdocs/library ] && ln -s $LIBRARY /usr/share/mympd/htdocs/library
+  [ "$LIBRARY" != "" ] && [ ! -e /usr/share/mympd/htdocs/library ] && ln -s "$LIBRARY" /usr/share/mympd/htdocs/library
 else
   echo "/etc/mpd.conf not found, you must link your musicdir manually to /usr/share/mympd/htdocs/library"
 fi
