9272b69c9f531a6b72e8ebe795d1bf531ded3771
jcorporation <mail@jcgames.de>
2018-09-20 21:36:38 +0100

    Fix: link/remove library and pics

--- mkclean.sh
+++ mkclean.sh
@@ -5,4 +5,6 @@ rm -rf debian/tmp
 rm -f debian/files
 rm -f htdocs/js/bootstrap-native-v4.min.js
 rm -f htdocs/css/bootstrap.min.css
+rm -f htdocs/library
+rm -f htdocs/pics
 find ./ -name \*~ -delete
--- mkdebug.sh
+++ mkdebug.sh
@@ -1,7 +1,19 @@
 #/bin/sh
 
-cp dist/htdocs/js/bootstrap-native-v4.min.js htdocs/js/
+cp dist/htdocs/js/bootstrap-native-v4.js htdocs/js/bootstrap-native-v4.min.js
 cp dist/htdocs/css/bootstrap.min.css htdocs/css/
+
+echo "Trying to link musicdir to library"
+if [ -f /etc/mpd.conf ]
+then
+  LIBRARY=$(grep ^music_directory /etc/mpd.conf | awk {'print $2'} | sed -e 's/"//g')
+  [ "$LIBRARY" != "" ] && [ ! -e htdocs/library ] && ln -s "$LIBRARY" htdocs/library
+else
+  echo "/etc/mpd.conf not found, you must link your music_directory manually to htdocs/library"
+fi
+
+echo "Linking pics directory"
+[ -e htdocs/pics ] || ln -s /var/lib/mympd/pics htdocs/
 
 [ -d debug ] || mkdir debug
 cd debug
