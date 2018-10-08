61adcb1c59ffc19cf52c06b38452e9891f9f83a0
jcorporation <mail@jcgames.de>
2018-10-08 21:58:25 +0100

    Fix: get current song only on song change

--- htdocs/js/mympd.js
+++ htdocs/js/mympd.js
@@ -1055,7 +1055,8 @@ function parseState(obj) {
     setCounter(obj.data.currentSongId, obj.data.totalTime, obj.data.elapsedTime);
     
     //Get current song
-    sendAPI({"cmd": "MPD_API_PLAYER_CURRENT_SONG"}, songChange);
+    if (lastState && lastState.data.currentSongId != obj.data.currentSongId)
+        sendAPI({"cmd": "MPD_API_PLAYER_CURRENT_SONG"}, songChange);
     //clear playback card if not playing
     if (obj.data.songPos == '-1') {
         domCache.currentTrack.innerText = 'Not playing';
