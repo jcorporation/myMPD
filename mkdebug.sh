ccb2ab50c6c4950d0f47c66e3059161924af91cb
jcorporation <mail@jcgames.de>
2019-03-07 22:33:35 +0000

    Fix: don't symlink pics and library anymore

--- mkdebug.sh
+++ mkdebug.sh
@@ -8,18 +8,6 @@
 [ -e $PWD/htdocs/css/mympd.min.css ] || ln -s $PWD/htdocs/css/mympd.css $PWD/htdocs/css/mympd.min.css
 [ -e $PWD/htdocs/css/bootstrap.min.css ] || ln -s $PWD/dist/htdocs/css/bootstrap.min.css $PWD/htdocs/css/bootstrap.min.css
 
-echo "Trying to link musicdir to library"
-if [ -f /etc/mpd.conf ]
-then
-  LIBRARY=$(grep ^music_directory /etc/mpd.conf | awk {'print $2'} | sed -e 's/"//g')
-  [ "$LIBRARY" != "" ] && [ ! -e htdocs/library ] && ln -s "$LIBRARY" htdocs/library
-else
-  echo "/etc/mpd.conf not found, you must link your music_directory manually to htdocs/library"
-fi
-
-echo "Linking pics directory"
-[ -e $PWD/htdocs/pics ] || ln -s /var/lib/mympd/pics htdocs/
-
 install -d debug
 cd debug
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=DEBUG ..
