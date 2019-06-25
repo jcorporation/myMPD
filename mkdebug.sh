1426134f0ce4615fd43cf0bc45d01df3e08bd015
jcorporation <mail@jcgames.de>
2019-06-25 20:48:53 +0100

    Fix: small translation framework improvements

--- mkdebug.sh
+++ mkdebug.sh
@@ -3,7 +3,6 @@
 [ -e "$PWD/htdocs/js/mympd.min.js" ] || ln -s "$PWD/htdocs/js/mympd.js" "$PWD/htdocs/js/mympd.min.js"
 [ -e "$PWD/htdocs/js/bootstrap-native-v4.min.js" ] || ln -s "$PWD/dist/htdocs/js/bootstrap-native-v4.js" "$PWD/htdocs/js/bootstrap-native-v4.min.js"
 [ -e "$PWD/htdocs/js/keymap.min.js" ] || ln -s "$PWD/htdocs/js/keymap.js" "$PWD/htdocs/js/keymap.min.js"
-[ -e "$PWD/htdocs/js/i18n.min.js" ] || ln -s "$PWD/dist/htdocs/js/i18n.min.js" "$PWD/htdocs/js/i18n.min.js"
 [ -e "$PWD/htdocs/css/mympd.min.css" ] || ln -s "$PWD/htdocs/css/mympd.css" "$PWD/htdocs/css/mympd.min.css"
 [ -e "$PWD/htdocs/css/bootstrap.min.css" ] || ln -s "$PWD/dist/htdocs/css/bootstrap.min.css" "$PWD/htdocs/css/bootstrap.min.css"
 
@@ -13,4 +12,4 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=DEBUG ..
 make VERBOSE=1
 
 cd ../src/i18n || exit 1
-./tojson.pl pretty > ../../dist/htdocs/js/i18n.min.js
+./tojson.pl pretty > ../../htdocs/js/i18n.min.js
