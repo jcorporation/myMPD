82457c76bad6889129cc598e073d521bb6f4dfee
jcorporation <mail@jcgames.de>
2019-09-03 23:19:43 +0100

    Fix: add script for backward compatibility with current dietpi sourcebuild

---
+++ mkrelease.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+#Wrapper for build.sh for compatibility with dietpi source build
+#
+
+echo "WARNING: this script is deprecated use ./build.sh instead"
+./build.sh releaseinstall
+echo "WARNING: this script is deprecated use ./build.sh instead"
