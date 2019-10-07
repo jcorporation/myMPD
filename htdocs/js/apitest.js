3804be276346a99b0a94cc48c1d42f8d889e63a4
jcorporation <mail@jcgames.de>
2019-10-08 00:06:10 +0200

    Feat: rewrite apitest to test jsonrpc 2.0 api

--- htdocs/js/apitest.js
+++ htdocs/js/apitest.js
@@ -40,74 +40,74 @@ var time_end = 0;
 var time_all = 0;
 
 var cmds = [
-    '{"cmd":"MYMPD_API_BOOKMARK_SAVE","data":{"id": -1, "name":"testdir1", "uri":"parent1/child1", "type": "dir"}}',
-    '{"cmd":"MYMPD_API_BOOKMARK_SAVE","data":{"id": -1, "name":"testdir2", "uri":"parent2/child2", "type": "dir"}}',
-    '{"cmd":"MYMPD_API_BOOKMARK_SAVE","data":{"id": 2, "name":"testdir2id2", "uri":"parent1/child1/child2", "type": "dir"}}',
-    '{"cmd":"MYMPD_API_BOOKMARK_LIST","data":{"offset":0}}',
-    '{"cmd":"MYMPD_API_BOOKMARK_RM","data":{"id":1}}',
-    '{"cmd":"MPD_API_QUEUE_CLEAR"}',
-    '{"cmd":"MPD_API_DATABASE_SEARCH","data":{"offset":0,"filter":"any","searchstr":"__SEARCHSTR__","plist":"","cols":["Title","Album"]}}',
-    '{"cmd":"MPD_API_QUEUE_ADD_TRACK","data":{"uri":"__URI1__"}}',
-    '{"cmd":"MPD_API_DATABASE_SEARCH","data":{"offset":0,"filter":"any","searchstr":"__SEARCHSTR__","plist":"queue","cols":["Title","Album"]}}',
-    '{"cmd":"MPD_API_QUEUE_LIST","data":{"offset":0,"cols":["Title","Album"]}}',
-    '{"cmd":"MPD_API_QUEUE_MOVE_TRACK","data":{"from":1,"to":2}}',
-    '{"cmd":"MPD_API_QUEUE_ADD_RANDOM","data":{"playlist":"Database","quantity":2, "mode":1}}',
-    '{"cmd":"MPD_API_QUEUE_ADD_RANDOM","data":{"playlist":"Database","quantity":1, "mode":2}}',
-    '{"cmd":"MPD_API_QUEUE_RM_RANGE","data":{"start":1,"end":-1}}',
-    '{"cmd":"MPD_API_PLAYER_PLAY_TRACK","data":{"track":__TRACKID__}}',
-    '{"cmd":"MPD_API_PLAYER_SEEK","data":{"songid":__TRACKID__,"seek":10}}',
-    '{"cmd":"MPD_API_QUEUE_RM_TRACK","data":{"track":__TRACKID__}}',
-    '{"cmd":"MPD_API_QUEUE_SAVE","data":{"plist":"test"}}',
-    '{"cmd":"MPD_API_QUEUE_SEARCH","data":{"offset":0,"filter":"any","searchstr":"__SEARCHSTR__","cols":["Title","Album"]}}',
-    '{"cmd":"MPD_API_QUEUE_ADD_PLAY_TRACK","data":{"uri":"__URI2__"}}',
-    '{"cmd":"MPD_API_QUEUE_SHUFFLE"}',
-    '{"cmd":"MPD_API_QUEUE_REPLACE_TRACK","data":{"uri":"__URI1__"}}',
-    '{"cmd":"MPD_API_QUEUE_ADD_TRACK_AFTER","data":{"uri":"__URI1__","to":1}',
-    '{"cmd":"MPD_API_PLAYLIST_RENAME","data":{"from":"test","to":"test2"}}',
-    '{"cmd":"MPD_API_PLAYLIST_CONTENT_LIST","data":{"uri":"test2","offset":0,"filter":"","cols":["Title","Album"]}}',
-    '{"cmd":"MPD_API_QUEUE_ADD_PLAYLIST","data":{"plist":"test2"}}',
-    '{"cmd":"MPD_API_QUEUE_REPLACE_PLAYLIST","data":{"plist":"test2"}}',
-    '{"cmd":"MPD_API_PLAYLIST_LIST","data":{"offset":0,"filter":""}}',
-    '{"cmd":"MPD_API_PLAYLIST_CLEAR","data":{"uri":"test2"}}',
-    '{"cmd":"MPD_API_DATABASE_SEARCH","data":{"offset":0,"filter":"any","searchstr":"__SEARCHSTR__","plist":"test2","cols":["Title","Album","Artist"]}}',
-    '{"cmd":"MPD_API_QUEUE_ADD_TRACK","data":{"uri":"__URI1__"}}',
-    '{"cmd":"MPD_API_QUEUE_ADD_TRACK","data":{"uri":"__URI2__"}}',
-    '{"cmd":"MPD_API_PLAYER_PLAY"}',
-    '{"cmd":"MPD_API_QUEUE_CROP"}',
-    '{"cmd":"MPD_API_QUEUE_LAST_PLAYED","data":{"offset":0,"cols":["Artist","Album","AlbumArtist"]}}',
-    '{"cmd":"MPD_API_PLAYLIST_ADD_TRACK","data":{"plist":"test2","uri":"__URI1__"}}',
-    '{"cmd":"MPD_API_PLAYLIST_ADD_TRACK","data":{"plist":"test2","uri":"__URI1__"}}',
-    '{"cmd":"MPD_API_PLAYLIST_MOVE_TRACK","data":{"plist":"test2","from":1,"to":2}}',
-    '{"cmd":"MPD_API_PLAYLIST_RM_TRACK","data":{"uri":"test2","track":1}',
-    '{"cmd":"MPD_API_PLAYLIST_RM","data":{"uri":"test2"}}',
-    '{"cmd":"MPD_API_DATABASE_SEARCH_ADV","data":{"offset":0,"expression":"(any contains \'__SEARCHSTR__\')","sort":"Title", "sortdesc":false,"plist":"","cols":["Title","Album","Artist"]}}',
-    '{"cmd":"MPD_API_DATABASE_UPDATE"}',
-//    '{"cmd":"MPD_API_DATABASE_RESCAN"}',
-    '{"cmd":"MPD_API_DATABASE_FILESYSTEM_LIST","data":{"offset":0,"filter":"","path":"","cols":["Title","Album"]}}',
-    '{"cmd":"MPD_API_DATABASE_TAG_LIST","data":{"offset":0,"filter":"","tag":"Artist"}}',
-    '{"cmd":"MPD_API_DATABASE_TAG_ALBUM_LIST","data":{"offset":0,"filter":"","search":"__ARTIST1__","tag":"Artist"}}',
-    '{"cmd":"MPD_API_DATABASE_TAG_ALBUM_TITLE_LIST","data":{"album":"__ALBUM1__","search":"__ARTIST1__","tag":"Artist","cols":["Title","Album"]}}',
-    '{"cmd":"MPD_API_DATABASE_STATS"}',
-    '{"cmd":"MPD_API_DATABASE_SONGDETAILS","data":{"uri":"__URI1__"}}',
-    '{"cmd":"MPD_API_PLAYER_VOLUME_SET","data":{"volume":30}}',
-    '{"cmd":"MPD_API_PLAYER_VOLUME_GET"}',
-    '{"cmd":"MPD_API_PLAYER_NEXT"}',
-    '{"cmd":"MPD_API_PLAYER_CURRENT_SONG"}',
-    '{"cmd":"MPD_API_PLAYER_PREV"}',
-    '{"cmd":"MPD_API_PLAYER_PAUSE"}',
-    '{"cmd":"MPD_API_PLAYER_STOP"}',
-    '{"cmd":"MPD_API_PLAYER_OUTPUT_LIST"}',
-    '{"cmd":"MPD_API_PLAYER_TOGGLE_OUTPUT","data":{"output":"__OUTPUTID__","state":1}}',
-    '{"cmd":"MPD_API_PLAYER_STATE"}',
-    '{"cmd":"MYMPD_API_SETTINGS_GET"}',
-    '{"cmd":"MPD_API_SETTINGS_GET"}',
-    '{"cmd":"MYMPD_API_SETTINGS_SET","data":{"random": 0}}',
-    '{"cmd":"MPD_API_LIKE","data":{"uri":"__URI2__","like":2}}',
-    '{"cmd":"MYMPD_API_SYSCMD","data":{"cmd": "Echo"}}',
-    '{"cmd":"MYMPD_API_COLS_SAVE","data":{"table":"colsPlayback","cols":["Artist","Album","AlbumArtist"]}}',
-    '{"cmd":"MPD_API_SMARTPLS_UPDATE_ALL"}',
-    '{"cmd":"MPD_API_SMARTPLS_SAVE","data":{"type":"newest","playlist":"myMPDsmart-newestSongs","timerange":2678400}}',
-    '{"cmd":"MPD_API_SMARTPLS_GET","data":{"playlist":"myMPDsmart-newestSongs"}}'
+    '{"jsonrpc":"2.0","id":0,"method":"MYMPD_API_BOOKMARK_SAVE","params":{"id": -1, "name":"testdir1", "uri":"parent1/child1", "type": "dir"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MYMPD_API_BOOKMARK_SAVE","params":{"id": -1, "name":"testdir2", "uri":"parent2/child2", "type": "dir"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MYMPD_API_BOOKMARK_SAVE","params":{"id": 2, "name":"testdir2id2", "uri":"parent1/child1/child2", "type": "dir"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MYMPD_API_BOOKMARK_LIST","params":{"offset":0}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MYMPD_API_BOOKMARK_RM","params":{"id":1}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_CLEAR"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_DATABASE_SEARCH","params":{"offset":0,"filter":"any","searchstr":"__SEARCHSTR__","plist":"","cols":["Title","Album"]}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_ADD_TRACK","params":{"uri":"__URI1__"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_DATABASE_SEARCH","params":{"offset":0,"filter":"any","searchstr":"__SEARCHSTR__","plist":"queue","cols":["Title","Album"]}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_LIST","params":{"offset":0,"cols":["Title","Album"]}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_MOVE_TRACK","params":{"from":1,"to":2}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_ADD_RANDOM","params":{"playlist":"Database","quantity":2, "mode":1}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_ADD_RANDOM","params":{"playlist":"Database","quantity":1, "mode":2}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_RM_RANGE","params":{"start":1,"end":-1}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYER_PLAY_TRACK","params":{"track":__TRACKID__}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYER_SEEK","params":{"songid":__TRACKID__,"seek":10}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_RM_TRACK","params":{"track":__TRACKID__}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_SAVE","params":{"plist":"test"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_SEARCH","params":{"offset":0,"filter":"any","searchstr":"__SEARCHSTR__","cols":["Title","Album"]}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_ADD_PLAY_TRACK","params":{"uri":"__URI2__"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_SHUFFLE"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_REPLACE_TRACK","params":{"uri":"__URI1__"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_ADD_TRACK_AFTER","params":{"uri":"__URI1__","to":1}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYLIST_RENAME","params":{"from":"test","to":"test2"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYLIST_CONTENT_LIST","params":{"uri":"test2","offset":0,"filter":"","cols":["Title","Album"]}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_ADD_PLAYLIST","params":{"plist":"test2"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_REPLACE_PLAYLIST","params":{"plist":"test2"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYLIST_LIST","params":{"offset":0,"filter":""}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYLIST_CLEAR","params":{"uri":"test2"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_DATABASE_SEARCH","params":{"offset":0,"filter":"any","searchstr":"__SEARCHSTR__","plist":"test2","cols":["Title","Album","Artist"]}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_ADD_TRACK","params":{"uri":"__URI1__"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_ADD_TRACK","params":{"uri":"__URI2__"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYER_PLAY"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_CROP"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_QUEUE_LAST_PLAYED","params":{"offset":0,"cols":["Artist","Album","AlbumArtist"]}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYLIST_ADD_TRACK","params":{"plist":"test2","uri":"__URI1__"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYLIST_ADD_TRACK","params":{"plist":"test2","uri":"__URI1__"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYLIST_MOVE_TRACK","params":{"plist":"test2","from":1,"to":2}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYLIST_RM_TRACK","params":{"uri":"test2","track":1}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYLIST_RM","params":{"uri":"test2"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_DATABASE_SEARCH_ADV","params":{"offset":0,"expression":"(any contains \'__SEARCHSTR__\')","sort":"Title", "sortdesc":false,"plist":"","cols":["Title","Album","Artist"]}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_DATABASE_UPDATE"}',
+//    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_DATABASE_RESCAN"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_DATABASE_FILESYSTEM_LIST","params":{"offset":0,"filter":"","path":"","cols":["Title","Album"]}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_DATABASE_TAG_LIST","params":{"offset":0,"filter":"","tag":"Artist"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_DATABASE_TAG_ALBUM_LIST","params":{"offset":0,"filter":"","search":"__ARTIST1__","tag":"Artist"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_DATABASE_TAG_ALBUM_TITLE_LIST","params":{"album":"__ALBUM1__","search":"__ARTIST1__","tag":"Artist","cols":["Title","Album"]}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_DATABASE_STATS"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_DATABASE_SONGDETAILS","params":{"uri":"__URI1__"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYER_VOLUME_SET","params":{"volume":30}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYER_VOLUME_GET"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYER_NEXT"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYER_CURRENT_SONG"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYER_PREV"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYER_PAUSE"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYER_STOP"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYER_OUTPUT_LIST"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYER_TOGGLE_OUTPUT","params":{"output":"__OUTPUTID__","state":1}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_PLAYER_STATE"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_LIKE","params":{"uri":"__URI2__","like":2}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MYMPD_API_SETTINGS_GET"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_SETTINGS_GET"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MYMPD_API_SETTINGS_SET","params":{"random": 0}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MYMPD_API_SYSCMD","params":{"cmd": "Echo"}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MYMPD_API_COLS_SAVE","params":{"table":"colsPlayback","cols":["Artist","Album","AlbumArtist"]}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_SMARTPLS_UPDATE_ALL"}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_SMARTPLS_SAVE","params":{"type":"newest","playlist":"myMPDsmart-newestSongs","timerange":2678400}}',
+    '{"jsonrpc":"2.0","id":0,"method":"MPD_API_SMARTPLS_GET","params":{"playlist":"myMPDsmart-newestSongs"}}'
 ];
 
 function setTest(cmd, state, response) {
@@ -136,26 +136,30 @@ function sendAPI(request, callback) {
                 try {
                     obj = JSON.parse(ajaxRequest.responseText);
                     time_end = new Date().getTime();
-                    if (!obj.type)
+                    if (!obj.error && !obj.result && !obj.jsonrpc) {
                         setTest(request, 'error', 'Invalid response: ' + ajaxRequest.responseText);
+                    }
                     else {
-                        if (obj.type == 'search' && obj.data.length > 1) {
-                            uri1 = obj.data[0].uri;
-                            artist1 = obj.data[0].Artist;
-                            album1 = obj.data[0].Album;
-                            uri2 = obj.data[1].uri;
-                            artist2 = obj.data[1].Artist;
-                            album2 = obj.data[1].Album;
-                        }
-                        else if (obj.type == 'queue' && obj.data.length > 0)
-                            trackId = obj.data[0].id;
-                        else if (obj.type == 'outputs' && obj.data.length > 0)
-                            ouputId = obj.data[0].id;
-                        
-                        if (obj.type == 'error')
-                            setTest(request, 'error', ajaxRequest.responseText);
-                        else 
+                        if (obj.result) {
+                            if (obj.result.method == 'MPD_API_DATABASE_SEARCH' && obj.result.data && obj.result.data.length > 1) {
+                                uri1 = obj.result.data[0].uri;
+                                artist1 = obj.result.data[0].Artist;
+                                album1 = obj.result.data[0].Album;
+                                uri2 = obj.result.data[1].uri;
+                                artist2 = obj.result.data[1].Artist;
+                                album2 = obj.result.data[1].Album;
+                            }
+                            else if (obj.result.method == 'MPD_API_QUEUE_LIST' && obj.result.data.length > 0) {
+                                trackId = obj.result.data[0].id;
+                            }
+                            else if (obj.result.method == 'MPD_API_PLAYER_OUTPUT_LIST' && obj.result.data.length > 0) {
+                                outputId = obj.result.data[0].id;
+                            }
                             setTest(request, 'ok', ajaxRequest.responseText);
+                        } 
+                        else {
+                            setTest(request, 'error', ajaxRequest.responseText);
+                        }
                     }
                 }
                 catch(e) {
@@ -166,10 +170,12 @@ function sendAPI(request, callback) {
                 setTest(request, 'error', ajaxRequest.responseText);
             }
             i++;
-            if (i < cmds.length)
+            if (i < cmds.length) {
                 sendAPI(cmds[i]);
-            else
+            }
+            else {
                 document.getElementsByTagName('h2')[0].innerText = 'Finished';
+            }
         }
     };
     request = request.replace(/__TRACKID__/, trackId);
