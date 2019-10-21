b039b692cc795bdd30949f186bb2e36cf400a8f2
jcorporation <mail@jcgames.de>
2019-10-22 00:37:29 +0100

    Fix: update dist files

--- dist/htdocs/js/mympd.js
+++ dist/htdocs/js/mympd.js
@@ -3953,10 +3953,10 @@ function clickTitle() {
 */
 
 function focusTable(rownr, table) {
-    if (table === null) {
+    if (table === undefined) {
         table = document.getElementById(app.current.app + (app.current.tab !== undefined ? app.current.tab : '') + (app.current.view !== undefined ? app.current.view : '') + 'List');
         //support for BrowseDatabaseAlbum list
-        if (table === null) {
+        if (table === undefined) {
             table = document.getElementById(app.current.app + app.current.tab + 'TagList');
         }
         //support for BrowseDatabaseAlbum cards
@@ -3967,7 +3967,7 @@ function focusTable(rownr, table) {
         }
     }
 
-    if (table !== null) {
+    if (table !== undefined) {
         let sel = table.getElementsByClassName('selected');
         if (rownr === undefined) {
             if (sel.length === 0) {
