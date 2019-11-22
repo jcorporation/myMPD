aafaed3401149cfdfbde6488248ee79340fff533
Juergen Mang <mail@jcgames.de>
2019-11-22 13:35:58 +0100

    Fix: pull libmympdclient from github

--- dist/src/libmpdclient/update.sh
+++ dist/src/libmpdclient/update.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
+STARTDIR=$(pwd)
+
+TMPDIR=$(mktemp -d)
+cd $TMPDIR || exit 1
+git clone -b libmympdclient https://github.com/jcorporation/libmpdclient.git
+cd libmpdclient || exit 1
+meson . output
+ninja -C output
+
+cd $STARTDIR || exit 1
 install -d src
 install -d include/mpd/
 
-rsync -av /data/src/libmpdclient/src/ ./src/
-rsync -av /data/src/libmpdclient/include/mpd/ ./include/mpd/
+rsync -av $TMPDIR/libmpdclient/src/ ./src/
+rsync -av $TMPDIR/libmpdclient/include/mpd/ ./include/mpd/
 
-sed -e 's/@MAJOR_VERSION@/2/' -e 's/@MINOR_VERSION@/17/' -e 's/@PATCH_VERSION@/0/' \
-  /data/src/libmpdclient/include/mpd/version.h.in > include/mpd/version.h
-  
-cp /data/src/libmpdclient/output/config.h include/config.h
+rsync -av $TMPDIR/libmpdclient/output/version.h include/mpd/version.h
+rsync -av $TMPDIR/libmpdclient/output/config.h include/config.h
+
 
