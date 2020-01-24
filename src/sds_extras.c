6c1e31d1eaaa8d9d56afde88162f4a53a30dcf7e
Juergen Mang <mail@jcgames.de>
2020-01-24 14:23:09 +0100

    Fix: repair json escaping issues

--- src/sds_extras.c
+++ src/sds_extras.c
@@ -12,9 +12,23 @@
 #include "../dist/src/sds/sds.h"
 #include "sds_extras.h"
 
+static int json_get_utf8_char_len(unsigned char ch) {
+  if ((ch & 0x80) == 0) return 1;
+  switch (ch & 0xf0) {
+    case 0xf0:
+      return 4;
+    case 0xe0:
+      return 3;
+    default:
+      return 2;
+  }
+}
+
 sds sdscatjson(sds s, const char *p, size_t len) {
+    const char *hex_digits = "0123456789abcdef";
+    size_t cl = 0;
     s = sdscatlen(s, "\"", 1);
-    while(len--) {
+    while (len--) {
         switch(*p) {
         case '\\':
         case '"':
@@ -23,15 +37,24 @@ sds sdscatjson(sds s, const char *p, size_t len) {
         case '\n': s = sdscatlen(s, "\\n", 2);     break;
         case '\r': s = sdscatlen(s, "\\r", 2);     break;
         case '\t': s = sdscatlen(s, "\\t", 2);     break;
-        case '\a': s = sdscatlen(s, "\\a", 2);     break;
         case '\b': s = sdscatlen(s, "\\b", 2);     break;
+        case '\f': s = sdscatlen(s, "\\f", 2);     break;
         // Escape < to prevent script execution
         case '<' : s = sdscatlen(s, "\\u003C", 6); break;
+        //ignore vertical tabulator and alert
+        case '\v': 
+        case '\a': 
+            break;
         default:
-//            if (isprint(*p))
+            if (isprint(*p)) {
                 s = sdscatprintf(s, "%c", *p);
-//            else
-//                s = sdscatprintf(s,"\\u%04X",(unsigned char)*p);
+            }
+            else if ((cl = json_get_utf8_char_len(*p)) == 1) {
+                s = sdscatprintf(s, "\\u00%s%s", &hex_digits[(*p >> 4) % 0xf], &hex_digits[*p % 0xf]);
+            } 
+            else {
+                s = sdscatprintf(s, "%c", *p);
+            }
             break;
         }
         p++;
