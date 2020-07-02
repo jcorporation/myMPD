e0d9a9d418b8ed4a6a3f2716bcff316b322641d8
jcorporation <mail@juergenmang.de>
2020-07-02 22:52:33 +0200

    Fix: escaping function escapes now the characters <>"' and according unicode values

--- htdocs/js/locale.js
+++ htdocs/js/locale.js
@@ -7,10 +7,16 @@
 
 function e(x) {
     if (isNaN(x)) {
-        return x.replace(/([<>"])/g, function(m0, m1) {
+        return x.replace(/([<>"'])/g, function(m0, m1) {
             if (m1 === '<') return '&lt;';
             else if (m1 === '>') return '&gt;';
             else if (m1 === '"') return '&quot;';
+            else if (m1 === '\'') return '&apos;';
+        }).replace(/\\u(003C|003E|0022|0027)/gi, function(m0, m1) {
+            if (m1 === '003C') return '&lt;';
+            else if (m1 === '003E') return '&gt;';
+            else if (m1 === '0022') return '&quot;';
+            else if (m1 === '0027') return '&apos;';
         });
     }
     else {
