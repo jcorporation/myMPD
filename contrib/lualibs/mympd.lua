7ccedc8ab620bdd5bba5aa8971cd532e28256ce3
jcorporation <mail@juergenmang.de>
2020-08-16 23:22:20 +0200

    Feat: add mympd.os_capture lua function to capture os commands output

--- contrib/lualibs/mympd.lua
+++ contrib/lualibs/mympd.lua
@@ -6,10 +6,42 @@
 -- https://github.com/jcorporation/mympd
 --
 
-mympd = { _version = "0.1.0" }
+mympd = { _version = "0.2.0" }
 
+--
+-- Function to retriebe mympd_state
+-- 
 function mympd.init()
     return mympd_api("MPD_API_SCRIPT_INIT")
+end
+
+--
+-- Function to retrieve console output
+-- 
+function mympd.os_capture(cmd, raw)
+    if io then
+    	local handle = assert(io.popen(cmd, 'r'))
+    	local output = assert(handle:read('*a'))
+    
+    	handle:close()
+    
+    	if raw then 
+            return output 
+    	end
+   
+    	output = string.gsub(
+            string.gsub(
+                string.gsub(output, '^%s+', ''), 
+            	'%s+$', 
+            	''
+            ), 
+            '[\n\r]+',
+            ' '
+    	)
+	return output
+    else
+    	return "io library must be loaded"
+    end
 end
 
 return mympd
