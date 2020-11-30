37b37e2f0e062f5f1747cc50f9fbfc180e23a437
jcorporation <mail@jcgames.de>
2020-11-30 21:46:25 +0100

    Upd: set version to 6.9.0

--- htdocs/sw.js
+++ htdocs/sw.js
@@ -4,7 +4,7 @@
  https://github.com/jcorporation/mympd
 */
 
-var CACHE = 'myMPD-cache-v6.8.3';
+var CACHE = 'myMPD-cache-v6.9.0';
 var subdir = self.location.pathname.replace('/sw.js', '').replace(/\/$/, '');
 var urlsToCache = [
     subdir + '/',
