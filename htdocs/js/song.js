08191badf1fa14f07fc9c1e4dfa1a0c28a3ec7b3
jcorporation <mail@jcgames.de>
2021-03-18 08:59:24 +0100

    Upd: improve layout of lyrics change buttons #449

--- htdocs/js/song.js
+++ htdocs/js/song.js
@@ -230,8 +230,8 @@ function getLyrics(uri, el) {
                 else {
                     ht = i;
                 }
-                lyricsHeader += '<label data-num="' + i + '" class="btn btn-sm btn-outline-secondary mr-2' + (i === 0 ? ' active' : '') + '" title="' + 
-                    (obj.result.data[i].synced === true ? t('Synced lyrics') : t('Unsynced lyrics')) + '">' + ht + '</label>';
+                lyricsHeader += '<label data-num="' + i + '" class="btn btn-sm btn-outline-secondary mr-2 lyricsChangeButton' + (i === 0 ? ' active' : '') + '" title="' + 
+                    (obj.result.data[i].synced === true ? t('Synced lyrics') : t('Unsynced lyrics')) + ': ' + e(ht) + '">' + e(ht) + '</label>';
                 lyrics += '<div class="lyricsText ' + (i > 0 ? 'hide' : '') + (obj.result.data[i].synced === true ? 'lyricsSyncedText' : '') + 
                     (clickable === true ? '' : ' fullHeight') + '">' +
                     (obj.result.data[i].synced === true ? parseSyncedLyrics(obj.result.data[i].text, clickable) : e(obj.result.data[i].text).replace(/\n/g, "<br/>")) + 
