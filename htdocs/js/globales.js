781f38c6d129f4327e3f949f40e1c07e082d0437
Juergen Mang <mail@jcgames.de>
2021-04-21 20:17:28 +0200

    Fix: some improvements

--- htdocs/js/globales.js
+++ htdocs/js/globales.js
@@ -459,7 +459,7 @@ const advancedSettingsDefault = {
         "inputType": "select",
         "title": "Locale",
         "form": "localeFrm",
-        "onChange": 'eventChangeLocale'
+        "onChange": "eventChangeLocale"
     }
 };
 
