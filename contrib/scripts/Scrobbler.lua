549683ad9168c56c30b22471027ca45959903360
jcorporation <mail@jcgames.de>
2021-06-06 22:49:19 +0200

    Upd: use json.encode to create payload

--- contrib/scripts/Scrobbler.lua
+++ contrib/scripts/Scrobbler.lua
@@ -6,14 +6,16 @@ headers = "Content-type: application/json\r\n"..
 rc, raw_result = mympd_api("MPD_API_PLAYER_CURRENT_SONG")
 if rc == 0 then
   current_song = json.decode(raw_result)
-  payload = "{"..
-    "\"listen_type\":\"single\","..
-    "\"payload\":[{"..
-      "\"listened_at\": "..current_song["result"]["startTime"]..","..
-      "\"track_metadata\":{"..
-        "\"artist_name\":\""..current_song["result"]["Artist"].."\","..
-        "\"track_name\":\""..current_song["result"]["Title"].."\""..
-      "}}]}"
+  payload = json.encode({
+    listen_type: "single",
+    payload: [{
+      listened_at: current_song["result"]["startTime"],
+      track_metadata: {
+        artist_name: current_song["result"]["Artist"],
+        track_name: current_song["result"]["Title"]
+      }
+    }]
+  });
   rc, response, header, body = mympd_api_http_client("POST", uri, headers, payload)
   if rc > 0 then
     return body
