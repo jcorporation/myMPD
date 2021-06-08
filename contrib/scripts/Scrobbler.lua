8b3e9e80034b7b26cf319eeb0b66cb420db18529
Juergen Mang <mail@jcgames.de>
2021-06-08 19:18:48 +0200

    Fix: json.encode

--- contrib/scripts/Scrobbler.lua
+++ contrib/scripts/Scrobbler.lua
@@ -3,18 +3,18 @@ uri = "https://api.listenbrainz.org/1/submit-listens"
 headers = "Content-type: application/json\r\n"..
   "Authorization: Token "..token.."\r\n"
 
-rc, raw_result = mympd_api("MPD_API_PLAYER_CURRENT_SONG")
+rc, raw_result = mympd_api("MYMPD_API_PLAYER_CURRENT_SONG")
 if rc == 0 then
   current_song = json.decode(raw_result)
   payload = json.encode({
-    listen_type: "single",
-    payload: [{
-      listened_at: current_song["result"]["startTime"],
-      track_metadata: {
-        artist_name: current_song["result"]["Artist"],
-        track_name: current_song["result"]["Title"]
+    listen_type = "single",
+    payload = {{
+      listened_at = current_song["result"]["startTime"],
+      track_metadata = {
+        artist_name = current_song["result"]["Artist"],
+        track_name = current_song["result"]["Title"]
       }
-    }]
+    }}
   });
   rc, response, header, body = mympd_api_http_client("POST", uri, headers, payload)
   if rc > 0 then
