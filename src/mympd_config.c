c1001ee3a5bc2366f5da9a018afbea1ca6d28ba0
jcorporation <mail@jcgames.de>
2021-07-16 20:05:02 +0100

    Fix: read of string environment variables

--- src/mympd_config.c
+++ src/mympd_config.c
@@ -137,7 +137,7 @@ static const char *mympd_getenv(const char *env_var, bool first_startup) {
     }
     if (first_startup == true) {
         MYMPD_LOG_INFO("Using environment variable \"%s\" with value \"%s\"", env_var, env_value);
-        return env_var;
+        return env_value;
     }
     MYMPD_LOG_INFO("Ignoring environment variable \"%s\" with value \"%s\"", env_var, env_value);
     return NULL;
