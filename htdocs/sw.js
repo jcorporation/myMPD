900539cff9cfbb6aca59072cf79e1b10360be301
jcorporation <mail@jcgames.de>
2021-08-01 19:42:06 +0200

    Upd: set version to 8.1.0

--- htdocs/sw.js
+++ htdocs/sw.js
@@ -2,7 +2,7 @@
 // myMPD (c) 2018-2021 Juergen Mang <mail@jcgames.de>
 // https://github.com/jcorporation/mympd
 
-const CACHE = 'myMPD-cache-v8.0.3';
+const CACHE = 'myMPD-cache-v8.1.0';
 const subdir = self.location.pathname.replace('/sw.js', '').replace(/\/$/, '');
 const urlsToCache = [
     subdir + '/',
