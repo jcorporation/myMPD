7c5ffce6d0e999ac087aadae9c9ad30108ffed0a
jcorporation <mail@jcgames.de>
2021-09-20 19:31:31 +0200

    Upd: remove obsolet function Upd: code improvements

--- src/lib/mimetype.c
+++ src/lib/mimetype.c
@@ -65,37 +65,22 @@ const char *get_ext_by_mime_type(const char *mime_type) {
     return p->extension;
 }
 
-const char *get_mime_type_by_magic(const char *filename) {
-    errno = 0;
-    FILE *fp = fopen(filename, OPEN_FLAGS_READ_BIN);
-    if (fp == NULL) {
-        MYMPD_LOG_ERROR("Can not open file \"%s\"", filename);
-        MYMPD_LOG_ERRNO(errno);
-        return sdsempty();
-    }
-    unsigned char binary_buffer[12];
-    size_t read = fread(binary_buffer, 1, sizeof(binary_buffer), fp);
-    MYMPD_LOG_DEBUG("Read %u bytes from file %s", read, filename);
-    fclose(fp);
-    sds stream = sdsnewlen(binary_buffer, read);
-    const char *mime_type = get_mime_type_by_magic_stream(stream);
-    FREE_SDS(stream);
-    return mime_type;
-}
-
 const char *get_mime_type_by_magic_stream(sds stream) {
     sds hex_buffer = sdsempty();
-    size_t len = sdslen(stream) < 12 ? sdslen(stream) : 12;
-    for (size_t i = 0; i < len; i++) {
+    size_t stream_len = sdslen(stream) < 12 ? sdslen(stream) : 12;
+    for (size_t i = 0; i < stream_len; i++) {
         hex_buffer = sdscatprintf(hex_buffer, "%02X", (unsigned char) stream[i]);
     }
     const struct t_mime_type_entry *p = NULL;
     for (p = mime_entries; p->magic_bytes != NULL; p++) {
         char *tmp_buffer = hex_buffer;
-        if (p->skip > 0 && sdslen(hex_buffer) < p->skip) {
+        if (p->skip > 0 && sdslen(hex_buffer) > p->skip) {
             tmp_buffer += p->skip;
         }
-        if (strncmp(tmp_buffer, p->magic_bytes, strlen(p->magic_bytes)) == 0) {
+        size_t magic_bytes_len = strlen(p->magic_bytes);
+        if (strlen(tmp_buffer) >= magic_bytes_len &&
+            strncmp(tmp_buffer, p->magic_bytes, magic_bytes_len) == 0)
+        {
             MYMPD_LOG_DEBUG("Matched magic bytes for mime_type: %s", p->mime_type);
             break;
         }
