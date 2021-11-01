305cba80c88f4c35656c833f6f2f2bd6b881a2db
jcorporation <mail@jcgames.de>
2021-11-01 13:36:02 +0100

    Rework notification filter

--- htdocs/js/notification.js
+++ htdocs/js/notification.js
@@ -58,25 +58,12 @@ const facilities = {
 function showNotification(title, text, facility, severity) {
     setStateIcon();
     logMessage(title, text, facility, severity);
-    
-    if (settings.webuiSettings.notifyWeb === true) {
-        //disabled notification for facility in advanced setting
-        let show = settings.webuiSettings['notification' + facilities[facility]];
-        if (show === null ) {
-            logDebug('Unknown facility: ' + facility);
-            //fallback to general
-            show = settings.webuiSettings['notificationGeneral'];    
-        }
-        if (show === false) { 
-            return;
-        }
-        const notification = new Notification(title, {icon: 'assets/favicon.ico', body: text});
-        setTimeout(notification.close.bind(notification), 3000);
-    }
-    
+   
     if (severity === 'info') {
         //notifications with severity info can be hidden
-        if (settings.notificationPage === false) { 
+        if (settings.webuiSettings.notifyPage === false &&
+            settings.webuiSettings.notifyWeb === false)
+        { 
             return;
         }
         //disabled notification for facility in advanced setting
@@ -89,6 +76,11 @@ function showNotification(title, text, facility, severity) {
         if (show === false) { 
             return;
         }
+    }
+
+    if (settings.webuiSettings.notifyWeb === true) {
+        const notification = new Notification(title, {icon: 'assets/favicon.ico', body: text});
+        setTimeout(notification.close.bind(notification), 3000);
     }
         
     if (alertTimeout) {
