197510a8e4e07634885b520df864f097717f5ed4
jcorporation <mail@jcgames.de>
2021-12-06 22:30:39 +0100

    Upd: use tojson_raw

--- src/mympd_api/mympd_api_browse.c
+++ src/mympd_api/mympd_api_browse.c
@@ -555,8 +555,8 @@ sds mympd_api_browse_album_list(struct t_mympd_state *mympd_state, sds buffer, s
             album = mpd_shared_get_tag_values(song, MPD_TAG_ALBUM, album);
             artist = mpd_shared_get_tag_values(song, mympd_state->mpd_state->tag_albumartist, artist);
             buffer = sdscat(buffer, "{\"Type\": \"album\",");
-            buffer = sdscatfmt(buffer, "\"Album\":%s,", album);
-            buffer = sdscatfmt(buffer, "\"AlbumArtist\":%s,", artist);
+            buffer = tojson_raw(buffer, "Album", album, true);
+            buffer = tojson_raw(buffer, "AlbumArtist:", artist, true);
             buffer = tojson_char(buffer, "FirstSongUri", mpd_song_get_uri(song), false);
             buffer = sdscatlen(buffer, "}", 1);
         }
