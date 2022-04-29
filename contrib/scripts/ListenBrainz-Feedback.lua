c6aa42915af3d9aeade5d15f10a9705f5cc453ce
jcorporation <mail@jcgames.de>
2022-04-29 19:43:45 +0200

    Fix: script arguments are uri and vote #394

--- contrib/scripts/ListenBrainz-Feedback.lua
+++ contrib/scripts/ListenBrainz-Feedback.lua
@@ -1,16 +1,22 @@
--- {"order":1,"arguments":["MUSICBRAINZ_TRACKID","feedback"]}
+-- {"order":1,"arguments":["uri","vote"]}
 mympd.init()
 uri = "https://api.listenbrainz.org/1/feedback/recording-feedback"
 headers = "Content-type: application/json\r\n"..
   "Authorization: Token "..mympd_state["listenbrainz_token"].."\r\n"
 
-feedback = arguments["feedback"] - 1
-
-payload = json.encode({
-  recording_mbid = arguments["MUSICBRAINZ_TRACKID"],
-  score = feedback
-});
-rc, response, header, body = mympd_api_http_client("POST", uri, headers, payload)
-if rc > 0 then
-  return body
+vote = arguments["vote"] - 1
+rc, raw_song = mympd_api("MYMPD_API_DATABASE_SONGDETAILS", "uri", arguments["uri"])
+if rc == 0 then
+  song = json.decode(raw_song)
+  mbid = song["result"]["MUSICBRAINZ_TRACKID"]
+  if mbid ~= nil then
+    payload = json.encode({
+      recording_mbid = mbid,
+      score = vote
+    });
+    rc, response, header, body = mympd_api_http_client("POST", uri, headers, payload)
+    if rc > 0 then
+      return body
+    end
+  end
 end
