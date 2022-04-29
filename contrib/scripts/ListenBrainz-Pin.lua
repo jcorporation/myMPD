d98558f5d2e6858a5b7f040a7e03aaab5bab8561
jcorporation <mail@jcgames.de>
2022-04-29 20:04:15 +0200

    Upd: accept song uri and lookup mbid like feedback script #394

--- contrib/scripts/ListenBrainz-Pin.lua
+++ contrib/scripts/ListenBrainz-Pin.lua
@@ -1,15 +1,22 @@
--- {"order":1,"arguments":["MUSICBRAINZ_TRACKID","blurb_content","pinned_until"]}
+-- {"order":1,"arguments":["uri","blurb_content","pinned_until"]}
 mympd.init()
 uri = "https://api.listenbrainz.org/1/pin"
 headers = "Content-type: application/json\r\n"..
   "Authorization: Token "..mympd_state["listenbrainz_token"].."\r\n"
 
-payload = json.encode({
-  recording_mbid = arguments["MUSICBRAINZ_TRACKID"],
-  blurb_content = arguments["blurb_content"],
-  pinned_until = arguments["pinned_until"]
-});
-rc, response, header, body = mympd_api_http_client("POST", uri, headers, payload)
-if rc > 0 then
-  return body
+rc, raw_song = mympd_api("MYMPD_API_DATABASE_SONGDETAILS", "uri", arguments["uri"])
+if rc == 0 then
+  song = json.decode(raw_song)
+  mbid = song["result"]["MUSICBRAINZ_TRACKID"]
+  if mbid ~= nil then
+    payload = json.encode({
+      recording_mbid = mbid,
+      blurb_content = arguments["blurb_content"],
+      pinned_until = arguments["pinned_until"]
+    });
+    rc, response, header, body = mympd_api_http_client("POST", uri, headers, payload)
+    if rc > 0 then
+      return body
+    end
+  end
 end
