9c2920dd1b507d19d30e475fe42b025a35a2b7b0
jcorporation <mail@jcgames.de>
2022-06-29 21:56:52 +0200

    Upd: add more utility tests

--- test/tests/test_utility.c
+++ test/tests/test_utility.c
@@ -7,7 +7,45 @@
 #include "mympd_config_defs.h"
 
 #include "../../dist/utest/utest.h"
+#include "../../src/lib/sds_extras.h"
 #include "../../src/lib/utility.h"
+
+UTEST(utility, test_getenv_check) {
+    setenv("TESTVAR", "testvalue", 0);
+    const char *testvar = getenv_check("TESTVAR", 20);
+    ASSERT_STREQ(testvar, "testvalue");
+
+    const char *tolong = getenv_check("TESTVAR", 5);
+    bool rc = tolong == NULL ? true : false;
+    ASSERT_TRUE(rc);
+    unsetenv("TESTVAR");
+}
+
+UTEST(utility, test_split_coverimage_names) {
+    sds names = sdsnew("cover, folder");
+    int count;
+    sds *array = split_coverimage_names(names, &count);
+    ASSERT_EQ(count, 2);
+    ASSERT_STREQ(array[0], "cover");
+    ASSERT_STREQ(array[1], "folder");
+    sdsfree(names);
+    sdsfreesplitres(array, count);
+}
+
+UTEST(utility, test_testdir) {
+    int rc = testdir("tmp", "/tmp", false);
+    ASSERT_EQ(rc, DIR_EXISTS);
+
+    rc = testdir("tmp2", "/tmp2", false);
+    ASSERT_EQ(rc, DIR_NOT_EXISTS);
+
+    rc = testdir("tmp/tmp2", "/tmp/tmp2", true);
+    ASSERT_EQ(rc, DIR_CREATED);
+    rmdir("/tmp/tmp2");
+
+    rc = testdir("tmp/tmp2", "/tmp/tmp2/tmp2", true);
+    ASSERT_EQ(rc, DIR_CREATE_FAILED);
+}
 
 UTEST(utility, test_my_msleep) {
     struct timespec begin, end;
@@ -22,4 +60,61 @@ UTEST(utility, test_my_msleep) {
         rc = true;
     }
     ASSERT_TRUE(rc);
+}
+
+UTEST(utility, test_is_virtual_cuedir) {
+    sds dir = sdsnew(".");
+    sds file = sdsnew("test");
+    bool rc = is_virtual_cuedir(dir, file);
+    ASSERT_TRUE(rc);
+    sdsfree(dir);
+    sdsfree(file);
+
+    dir = sdsnew("..");
+    file = sdsnew("build");
+    rc = is_virtual_cuedir(dir, file);
+    ASSERT_FALSE(rc);
+    sdsfree(dir);
+    sdsfree(file);
+}
+
+UTEST(utility, test_is_stream) {
+    bool rc = is_streamuri("https://github.com");
+    ASSERT_TRUE(rc);
+
+    rc = is_streamuri("/github.com");
+    ASSERT_FALSE(rc);
+}
+
+UTEST(utility, test_write_data_to_file) {
+    sds file = sdsnew("/tmp/test");
+    const char *data ="asdfjlkasdfjklsafd";
+    size_t len = strlen(data);
+    bool rc = write_data_to_file(file, data, len);
+    ASSERT_TRUE(rc);
+    sdsfree(file);
+}
+
+UTEST(sds_extras, test_sds_getfile) {
+    sds line = sdsempty();
+    FILE *fp = fopen("/tmp/test", "r");
+    int rc = sds_getfile(&line, fp, 1000);
+    fclose(fp);
+    ASSERT_EQ(rc, 0);
+    ASSERT_STREQ(line, "asdfjlkasdfjklsafd");
+
+    fp = fopen("/tmp/test", "r");
+    rc = sds_getfile(&line, fp, 5);
+    fclose(fp);
+    ASSERT_EQ(rc, -2);
+    unlink("/tmp/test");
+    sdsfree(line);
+}
+
+UTEST(utility, test_get_extension_from_filename) {
+    const char *ext = get_extension_from_filename("test.txt");
+    ASSERT_STREQ(ext, "txt");
+
+    const char *wo = get_extension_from_filename("test");
+    ASSERT_TRUE(wo == NULL);
 }
