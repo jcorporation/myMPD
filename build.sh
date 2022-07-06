5e88017ea69a4da087f386dc46b87ff6bdc00208
jcorporation <mail@jcgames.de>
2022-07-07 00:39:54 +0200

    Fix: compile_commands.json

--- build.sh
+++ build.sh
@@ -413,7 +413,7 @@ builddebug() {
   make VERBOSE=1
   echo "Linking compilation database"
   sed -e 's/\\t/ /g' -e 's/-Wformat-truncation//g' -e 's/-Wformat-overflow=2//g' -e 's/-fsanitize=bounds-strict//g' \
-    -e 's/-static-libasan//g' -e 's/-Wno-stringop-overread//g' -e '/-fstack-clash-protection//g' \
+    -e 's/-static-libasan//g' -e 's/-Wno-stringop-overread//g' -e 's/-fstack-clash-protection//g' \
     compile_commands.json > ../src/compile_commands.json
 }
 
