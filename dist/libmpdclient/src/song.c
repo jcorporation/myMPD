57e493cda428288420b10c8ffce17660e28fc89d
jcorporation <mail@jcgames.de>
2022-07-10 14:07:14 +0200

    Upd: libmympdclient to 1.0.12

--- dist/libmpdclient/src/song.c
+++ dist/libmpdclient/src/song.c
@@ -38,81 +38,12 @@
 #include "iso8601.h"
 #include "uri.h"
 #include "iaf.h"
+#include "isong.h"
 
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-
-struct mpd_tag_value {
-	struct mpd_tag_value *next;
-
-	char *value;
-};
-
-struct mpd_song {
-	char *uri;
-
-	struct mpd_tag_value tags[MPD_TAG_COUNT];
-
-	/**
-	 * Duration of the song in seconds, or 0 for unknown.
-	 */
-	unsigned duration;
-
-	/**
-	 * Duration of the song in milliseconds, or 0 for unknown.
-	 */
-	unsigned duration_ms;
-
-	/**
-	 * Start of the virtual song within the physical file in
-	 * seconds.
-	 */
-	unsigned start;
-
-	/**
-	 * End of the virtual song within the physical file in
-	 * seconds.  Zero means that the physical song file is
-	 * played to the end.
-	 */
-	unsigned end;
-
-	/**
-	 * The POSIX UTC time stamp of the last modification, or 0 if
-	 * that is unknown.
-	 */
-	time_t last_modified;
-
-	/**
-	 * The position of this song within the queue.
-	 */
-	unsigned pos;
-
-	/**
-	 * The id of this song within the queue.
-	 */
-	unsigned id;
-
-	/**
-	 * The priority of this song within the queue.
-	 */
-	unsigned prio;
-
-#ifndef NDEBUG
-	/**
-	 * This flag is used in an assertion: when it is set, you must
-	 * not call mpd_song_feed() again.  It is a safeguard for
-	 * buggy callers.
-	 */
-	bool finished;
-#endif
-
-	/**
-	 * The audio format as reported by MPD's decoder plugin.
-	 */
-	struct mpd_audio_format audio_format;
-};
 
 static struct mpd_song *
 mpd_song_new(const char *uri)
