c900526248f8b0f2e2b7a95c9ed6a7309d622c9e
jcorporation <mail@jcgames.de>
2022-09-25 16:15:54 +0200

    Upd: correct MYMPD_API_PLAYER_OPTIONS_SET arguments

--- docs/scripting/scripts/EnableJukebox.lua
+++ docs/scripting/scripts/EnableJukebox.lua
@@ -1,6 +1,6 @@
 -- {"order":1,"arguments":[]}
 
 mympd_api("MYMPD_API_PLAYER_STOP");
-mympd_api("MYMPD_API_PLAYER_OPTIONS_SET", "jukeboxMode", 1, "jukeboxPlaylist", "Database", "jukeboxQueueLength", 1, "jukeboxUniqueTag", "Title", "jukeboxLastPlayed", 24, "consume", 1);
+mympd_api("MYMPD_API_PLAYER_OPTIONS_SET", "jukeboxMode", "song", "jukeboxPlaylist", "Database", "jukeboxQueueLength", 1, "jukeboxUniqueTag", "Title", "jukeboxLastPlayed", 24, "consume", "1");
 mympd_api("MYMPD_API_QUEUE_CLEAR");
 mympd_api("MYMPD_API_PLAYER_PLAY");
