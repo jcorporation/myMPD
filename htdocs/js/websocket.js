fff2e75b69e577c2fd86585475539a51d4364d0e
jcorporation <mail@jcgames.de>
2022-10-20 21:08:09 +0200

    Fix: refresh of queue after update_queue message

--- htdocs/js/websocket.js
+++ htdocs/js/websocket.js
@@ -85,7 +85,7 @@ function webSocketConnect() {
                     if (app.id === 'QueueCurrent' &&
                         obj.method === 'update_queue')
                     {
-                        getQueue(document.getElementById('searchQueueStr').getAttribute('value'));
+                        getQueue(document.getElementById('searchQueueStr').value);
                     }
                     parseState(obj);
                     break;
