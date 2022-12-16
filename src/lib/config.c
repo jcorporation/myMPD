f5ebd7d9b1aaedfbdcc8fb78eddbe4ef0e1482dd
jcorporation <mail@jcgames.de>
2022-12-16 20:47:23 +0100

    Upd: centralize environment reading

--- src/lib/config.c
+++ src/lib/config.c
@@ -7,11 +7,11 @@
 #include "compile_time.h"
 #include "src/lib/config.h"
 
+#include "src/lib/env.h"
 #include "src/lib/log.h"
 #include "src/lib/mem.h"
 #include "src/lib/sds_extras.h"
 #include "src/lib/state_files.h"
-#include "src/lib/utility.h"
 #include "src/lib/validate.h"
 
 #include <inttypes.h>
@@ -22,10 +22,9 @@
  * Private declarations
  */
 
-static const char *mympd_getenv(const char *env_var, bool first_startup);
-static sds mympd_getenv_string(const char *env_var, const char *default_value, validate_callback vcb, bool first_startup);
-static int mympd_getenv_int(const char *env_var, int default_value, int min, int max, bool first_startup);
-static bool mympd_getenv_bool(const char *env_var, bool default_value, bool first_startup);
+static sds startup_getenv_string(const char *env_var, const char *default_value, validate_callback vcb, bool first_startup);
+static int startup_getenv_int(const char *env_var, int default_value, int min, int max, bool first_startup);
+static bool startup_getenv_bool(const char *env_var, bool default_value, bool first_startup);
 
 /**
  * Public functions
@@ -85,7 +84,8 @@ void mympd_config_defaults_initial(struct t_config *config) {
 /**
  * Sets the default values for config struct
  * This function is used after reading command line arguments and
- * reads the environment variables
+ * reads the environment variables.
+ * Environment variables are only respected at first startup.
  * @param config pointer to config struct
  */
 void mympd_config_defaults(struct t_config *config) {
@@ -93,18 +93,18 @@ void mympd_config_defaults(struct t_config *config) {
         MYMPD_LOG_INFO("Reading environment variables");
     }
     //configurable with environment variables at first startup
-    config->http_host = mympd_getenv_string("MYMPD_HTTP_HOST", CFG_MYMPD_HTTP_HOST, vcb_isname, config->first_startup);
-    config->http_port = mympd_getenv_int("MYMPD_HTTP_PORT", CFG_MYMPD_HTTP_PORT, 0, MPD_PORT_MAX, config->first_startup);
+    config->http_host = startup_getenv_string("MYMPD_HTTP_HOST", CFG_MYMPD_HTTP_HOST, vcb_isname, config->first_startup);
+    config->http_port = startup_getenv_int("MYMPD_HTTP_PORT", CFG_MYMPD_HTTP_PORT, 0, MPD_PORT_MAX, config->first_startup);
     #ifdef MYMPD_ENABLE_SSL
-        config->ssl = mympd_getenv_bool("MYMPD_SSL", CFG_MYMPD_SSL, config->first_startup);
-        config->ssl_port = mympd_getenv_int("MYMPD_SSL_PORT", CFG_MYMPD_SSL_PORT, 0, MPD_PORT_MAX, config->first_startup);
-        config->ssl_san = mympd_getenv_string("MYMPD_SSL_SAN", CFG_MYMPD_SSL_SAN, vcb_isname, config->first_startup);
-        config->custom_cert = mympd_getenv_bool("MYMPD_CUSTOM_CERT", CFG_MYMPD_CUSTOM_CERT, config->first_startup);
+        config->ssl = startup_getenv_bool("MYMPD_SSL", CFG_MYMPD_SSL, config->first_startup);
+        config->ssl_port = startup_getenv_int("MYMPD_SSL_PORT", CFG_MYMPD_SSL_PORT, 0, MPD_PORT_MAX, config->first_startup);
+        config->ssl_san = startup_getenv_string("MYMPD_SSL_SAN", CFG_MYMPD_SSL_SAN, vcb_isname, config->first_startup);
+        config->custom_cert = startup_getenv_bool("MYMPD_CUSTOM_CERT", CFG_MYMPD_CUSTOM_CERT, config->first_startup);
         sds default_cert = sdscatfmt(sdsempty(), "%S/ssl/server.pem", config->workdir);
         sds default_key = sdscatfmt(sdsempty(), "%S/ssl/server.key", config->workdir);
         if (config->custom_cert == true) {
-            config->ssl_cert = mympd_getenv_string("MYMPD_SSL_CERT", default_cert, vcb_isfilepath, config->first_startup);
-            config->ssl_key = mympd_getenv_string("MYMPD_SSL_KEY", default_key, vcb_isfilepath, config->first_startup);
+            config->ssl_cert = startup_getenv_string("MYMPD_SSL_CERT", default_cert, vcb_isfilepath, config->first_startup);
+            config->ssl_key = startup_getenv_string("MYMPD_SSL_KEY", default_key, vcb_isfilepath, config->first_startup);
             FREE_SDS(default_cert);
             FREE_SDS(default_key);
         }
@@ -120,17 +120,17 @@ void mympd_config_defaults(struct t_config *config) {
         config->ssl_cert = sdsempty();
         config->ssl_key = sdsempty();
     #endif
-    config->acl = mympd_getenv_string("MYMPD_ACL", CFG_MYMPD_ACL, vcb_isname, config->first_startup);
-    config->scriptacl = mympd_getenv_string("MYMPD_SCRIPTACL", CFG_MYMPD_SCRIPTACL, vcb_isname, config->first_startup);
+    config->acl = startup_getenv_string("MYMPD_ACL", CFG_MYMPD_ACL, vcb_isname, config->first_startup);
+    config->scriptacl = startup_getenv_string("MYMPD_SCRIPTACL", CFG_MYMPD_SCRIPTACL, vcb_isname, config->first_startup);
     #ifdef MYMPD_ENABLE_LUA
-        config->lualibs = mympd_getenv_string("MYMPD_LUALIBS", CFG_MYMPD_LUALIBS, vcb_isalnum, config->first_startup);
+        config->lualibs = startup_getenv_string("MYMPD_LUALIBS", CFG_MYMPD_LUALIBS, vcb_isalnum, config->first_startup);
     #else
         config->lualibs = sdsempty();
     #endif
     config->loglevel = CFG_MYMPD_LOGLEVEL;
     config->pin_hash = sdsnew(CFG_MYMPD_PIN_HASH);
-    config->covercache_keep_days = mympd_getenv_int("MYMPD_COVERCACHE_KEEP_DAYS", CFG_COVERCACHE_KEEP_DAYS, COVERCACHE_AGE_MIN, COVERCACHE_AGE_MAX, config->first_startup);
-    config->save_caches = mympd_getenv_bool("MYMPD_SAVE_CACHES", CFG_MYMPD_SSL, config->save_caches);
+    config->covercache_keep_days = startup_getenv_int("MYMPD_COVERCACHE_KEEP_DAYS", CFG_COVERCACHE_KEEP_DAYS, COVERCACHE_AGE_MIN, COVERCACHE_AGE_MAX, config->first_startup);
+    config->save_caches = startup_getenv_bool("MYMPD_SAVE_CACHES", CFG_MYMPD_SSL, config->save_caches);
 }
 
 /**
@@ -166,30 +166,13 @@ bool mympd_config_rw(struct t_config *config, bool write) {
     config->loglevel = state_file_rw_int(config->workdir, "config", "loglevel", config->loglevel, LOGLEVEL_MIN, LOGLEVEL_MAX, write);
     config->save_caches = state_file_rw_bool(config->workdir, "config", "save_caches", config->save_caches, write);
     //overwrite configured loglevel
-    config->loglevel = mympd_getenv_int("MYMPD_LOGLEVEL", config->loglevel, LOGLEVEL_MIN, LOGLEVEL_MAX, write);
+    config->loglevel = getenv_int("MYMPD_LOGLEVEL", config->loglevel, LOGLEVEL_MIN, LOGLEVEL_MAX);
     return true;
 }
 
 /**
  * Private functions
  */
-
-/**
- * Reads environment variables on first startup
- * @param env_var variable name to read
- * @param first_startup true for first startup else false
- * @return environment value or NULL
- */
-static const char *mympd_getenv(const char *env_var, bool first_startup) {
-    const char *env_value = getenv_check(env_var, 100);
-    if (first_startup == true) {
-        return env_value;
-    }
-    if (env_value != NULL) {
-        MYMPD_LOG_INFO("Ignoring environment variable \"%s\" with value \"%s\"", env_var, env_value);
-    }
-    return NULL;
-}
 
 /**
  * Gets an environment variable as sds string
@@ -199,19 +182,10 @@ static const char *mympd_getenv(const char *env_var, bool first_startup) {
  * @param first_startup true for first startup else false
  * @return environment variable as sds string
  */
-static sds mympd_getenv_string(const char *env_var, const char *default_value, validate_callback vcb, bool first_startup) {
-    const char *env_value = mympd_getenv(env_var, first_startup);
-    if (env_value == NULL) {
-        return sdsnew(default_value);
-    }
-    sds value = sdsnew(env_value);
-    if (vcb == NULL ||
-        vcb(value) == true)
-    {
-        return value;
-    }
-    FREE_SDS(value);
-    return sdsnew(default_value);
+static sds startup_getenv_string(const char *env_var, const char *default_value, validate_callback vcb, bool first_startup) {
+    return first_startup == true 
+        ? getenv_string(env_var, default_value, vcb)
+        : sdsnew(default_value);
 }
 
 /**
@@ -223,17 +197,10 @@ static sds mympd_getenv_string(const char *env_var, const char *default_value, v
  * @param first_startup true for first startup else false
  * @return environment variable as integer
  */
-static int mympd_getenv_int(const char *env_var, int default_value, int min, int max, bool first_startup) {
-    const char *env_value = mympd_getenv(env_var, first_startup);
-    if (env_value == NULL) {
-        return default_value;
-    }
-    int value = (int)strtoimax(env_value, NULL, 10);
-    if (value >= min && value <= max) {
-        return value;
-    }
-    MYMPD_LOG_WARN("Invalid value for \"%s\" using default", env_var);
-    return default_value;
+static int startup_getenv_int(const char *env_var, int default_value, int min, int max, bool first_startup) {
+    return first_startup == true
+        ? getenv_int(env_var, default_value, min, max)
+        : default_value;
 }
 
 /**
@@ -244,8 +211,8 @@ static int mympd_getenv_int(const char *env_var, int default_value, int min, int
  * @param first_startup true for first startup else false
  * @return environment variable as bool
  */
-static bool mympd_getenv_bool(const char *env_var, bool default_value, bool first_startup) {
-    const char *env_value = mympd_getenv(env_var, first_startup);
-    return env_value != NULL ? strcmp(env_value, "true") == 0 ? true : false
-                             : default_value;
+static bool startup_getenv_bool(const char *env_var, bool default_value, bool first_startup) {
+    return first_startup == true
+        ? getenv_bool(env_var, default_value)
+        : default_value;
 }
---
+++ src/lib/env.c
@@ -0,0 +1,129 @@
+/*
+ SPDX-License-Identifier: GPL-3.0-or-later
+ myMPD (c) 2018-2022 Juergen Mang <mail@jcgames.de>
+ https://github.com/jcorporation/mympd
+*/
+
+#include "compile_time.h"
+#include "src/lib/env.h"
+
+#include "src/lib/log.h"
+#include "src/lib/sds_extras.h"
+
+#include <inttypes.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Private declarations
+ */
+static const char *getenv_check(const char *env_var);
+
+/**
+ * Public functions
+ */
+
+/**
+ * Gets an environment variable as sds string
+ * @param env_var variable name to read
+ * @param default_value default value if variable is not set
+ * @param vcb validation callback
+ * @return environment variable as sds string
+ */
+sds getenv_string(const char *env_var, const char *default_value, validate_callback vcb) {
+    const char *env_value = getenv_check(env_var);
+    if (env_value == NULL) {
+        return sdsnew(default_value);
+    }
+    sds value = sdsnew(env_value);
+    if (vcb == NULL ||
+        vcb(value) == true)
+    {
+        return value;
+    }
+    FREE_SDS(value);
+    return sdsnew(default_value);
+}
+
+/**
+ * Gets an environment variable as int
+ * @param env_var variable name to read
+ * @param default_value default value if variable is not set
+ * @param min minimum value (including)
+ * @param max maximum value (including)
+ * @return environment variable as integer
+ */
+int getenv_int(const char *env_var, int default_value, int min, int max) {
+    const char *env_value = getenv_check(env_var);
+    if (env_value == NULL) {
+        return default_value;
+    }
+    int value = (int)strtoimax(env_value, NULL, 10);
+    if (value >= min && value <= max) {
+        return value;
+    }
+    MYMPD_LOG_WARN("Invalid value for \"%s\" using default", env_var);
+    return default_value;
+}
+
+/**
+ * Gets an environment variable as int
+ * @param env_var variable name to read
+ * @param default_value default value if variable is not set
+ * @param min minimum value (including)
+ * @param max maximum value (including)
+ * @return environment variable as integer
+ */
+unsigned getenv_uint(const char *env_var, unsigned default_value, unsigned min, unsigned max) {
+    const char *env_value = getenv_check(env_var);
+    if (env_value == NULL) {
+        return default_value;
+    }
+    unsigned value = (unsigned)strtoumax(env_value, NULL, 10);
+    if (value >= min && value <= max) {
+        return value;
+    }
+    MYMPD_LOG_WARN("Invalid value for \"%s\" using default", env_var);
+    return default_value;
+}
+
+/**
+ * Gets an environment variable as bool
+ * @param env_var variable name to read
+ * @param default_value default value if variable is not set
+ * @param vcb validation callback
+ * @return environment variable as bool
+ */
+bool getenv_bool(const char *env_var, bool default_value) {
+    const char *env_value = getenv_check(env_var);
+    return env_value != NULL ? strcmp(env_value, "true") == 0 ? true : false
+                             : default_value;
+}
+
+/**
+ * Private functions
+ */
+
+/**
+ * Gets an environment variable and checks its length
+ * @param env_var environment variable name
+ * @return environment variable value or NULL if it is not set or to long
+ */
+static const char *getenv_check(const char *env_var) {
+    const char *env_value = getenv(env_var); /* Flawfinder: ignore */
+    if (env_value == NULL) {
+        MYMPD_LOG_DEBUG("Environment variable \"%s\" not set", env_var);
+        return NULL;
+    }
+    if (env_value[0] == '\0') {
+        MYMPD_LOG_DEBUG("Environment variable \"%s\" is empty", env_var);
+        return NULL;
+    }
+    if (strlen(env_value) > MAX_ENV_LENGTH) {
+        MYMPD_LOG_WARN("Environment variable \"%s\" is too long", env_var);
+        return NULL;
+    }
+    MYMPD_LOG_INFO("Got environment variable \"%s\" with value \"%s\"", env_var, env_value);
+    return env_value;
+}
+
