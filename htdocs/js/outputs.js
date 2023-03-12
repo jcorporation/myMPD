529723b34fd685e82ef67dc1becc7ba72070603d
jcorporation <mail@jcgames.de>
2023-03-12 15:58:38 +0100

    Feat: different outputs by icons, show plugin in title #1000

--- htdocs/js/outputs.js
+++ htdocs/js/outputs.js
@@ -60,8 +60,19 @@ function parseOutputs(obj) {
             continue;
         }
         const titlePhrase = Object.keys(obj.result.data[i].attributes).length > 0 ? 'Edit attributes' : 'Show attributes';
-        const btn = elCreateNodes('button', {"class": ["btn", "btn-secondary", "d-flex", "justify-content-between"], "id": "btnOutput" + obj.result.data[i].id}, [
-            elCreateText('span', {"class": ["mi", "align-self-center"]}, (obj.result.data[i].plugin === 'httpd' ? 'cast' : 'volume_up')),
+        let icon = 'speaker';
+        switch(obj.result.data[i].plugin) {
+            case 'fifo':     icon = 'read_more'; break;
+            case 'httpd':    icon = 'stream'; break;
+            case 'null':     icon = 'check_box_outline_blank'; break;
+            case 'pipe':     icon = 'terminal'; break;
+            case 'recorder': icon = 'voicemail'; break;
+            case 'shout':    icon = 'cast'; break;
+            case 'snapcast': icon = 'hub'; break;
+        }
+        const buttonTitle = tn('Plugin') + ': ' + tn(obj.result.data[i].plugin);
+        const btn = elCreateNodes('button', {"class": ["btn", "btn-secondary", "d-flex", "justify-content-between"], "title": buttonTitle, "id": "btnOutput" + obj.result.data[i].id}, [
+            elCreateText('span', {"class": ["mi", "align-self-center"]}, icon),
             elCreateText('span', {"class": ["mx-2", "align-self-center"]}, obj.result.data[i].name),
             elCreateText('a', {"class": ["mi", "align-self-center"], "data-title-phrase": titlePhrase}, 'settings')
         ]);
