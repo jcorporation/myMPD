96382dca1fee111f5b8987b681e95969e13f1596
jcorporation <mail@jcgames.de>
2023-06-18 00:04:50 +0200

    Fix: whitespace

--- htdocs/js/tags.js
+++ htdocs/js/tags.js
@@ -207,7 +207,7 @@ function printValue(key, value) {
                 case 'smartpls': return elCreateText('span', {"class": ["mi"]}, 'queue_music');
                 case 'plist':    return elCreateText('span', {"class": ["mi"]}, 'list');
                 case 'dir':      return elCreateText('span', {"class": ["mi"]}, 'folder_open');
-                case 'stream':	 return elCreateText('span', {"class": ["mi"]}, 'stream');
+                case 'stream':   return elCreateText('span', {"class": ["mi"]}, 'stream');
                 case 'webradio': return elCreateText('span', {"class": ["mi"]}, 'radio');
                 default:         return elCreateText('span', {"class": ["mi"]}, 'radio_button_unchecked');
             }
