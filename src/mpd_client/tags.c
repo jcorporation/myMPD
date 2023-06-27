1c7c3ec13b3669e7cdbb8445f4c80b67dc0d1f65
jcorporation <mail@jcgames.de>
2023-06-27 22:55:14 +0200

    Fix: print AlbumId only if tags are enabled

--- src/mpd_client/tags.c
+++ src/mpd_client/tags.c
@@ -306,15 +306,15 @@ sds get_song_tags(sds buffer, bool tags_enabled, const struct t_tags *tagcols,
             buffer = mpd_client_get_tag_values(song, tagcols->tags[tagnr], buffer);
             buffer = sdscatlen(buffer, ",", 1);
         }
+        sds albumid = album_cache_get_key(song);
+        buffer = tojson_sds(buffer, "AlbumId", albumid, true);
+        FREE_SDS(albumid);
     }
     else {
         buffer = sdscat(buffer, "\"Title\":");
         buffer = mpd_client_get_tag_values(song, MPD_TAG_TITLE, buffer);
         buffer = sdscatlen(buffer, ",", 1);
     }
-    sds albumid = album_cache_get_key(song);
-    buffer = tojson_sds(buffer, "AlbumId", albumid, true);
-    FREE_SDS(albumid);
     buffer = tojson_uint(buffer, "Duration", mpd_song_get_duration(song), true);
     buffer = tojson_time(buffer, "LastModified", mpd_song_get_last_modified(song), true);
     buffer = tojson_char(buffer, "uri", mpd_song_get_uri(song), false);
