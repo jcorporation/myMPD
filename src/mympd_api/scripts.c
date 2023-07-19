jcorporation/myMPD

commit 3abd2066f94cc50f5c8319e5af00a6d3b998c965

Fix: remove double free(), set correct cache building state on error

src/mympd_api/scripts.c
@@ -757,7 +757,6 @@ static void free_t_script_thread_arg(struct t_script_thread_arg *script_thread_a
     FREE_SDS(script_thread_arg->script_content);
     FREE_SDS(script_thread_arg->partition);
     list_free(script_thread_arg->arguments);
-    FREE_PTR(script_thread_arg);
 }
 
 /**

src/mympd_api/mympd_api_handler.c
@@ -152,6 +152,8 @@ void mympd_api_handler(struct t_partition_state *partition_state, struct t_work_
             if (async == false) {
                 response->data = jsonrpc_respond_message(response->data, request->cmd_id, request->id,
                         JSONRPC_FACILITY_GENERAL, JSONRPC_SEVERITY_ERROR, "Error starting worker thread");
+                mympd_state->mpd_state->album_cache.building = false;
+                mympd_state->mpd_state->sticker_cache.building = false;
             }
             break;
         case INTERNAL_API_ALBUMCACHE_SKIPPED:
@@ -326,7 +328,6 @@ void mympd_api_handler(struct t_partition_state *partition_state, struct t_work_
                 response->data = jsonrpc_respond_message(response->data, request->cmd_id, request->id,
                         JSONRPC_FACILITY_SCRIPT, JSONRPC_SEVERITY_ERROR, "Invalid script name");
                 list_free(arguments);
-                FREE_PTR(arguments);
             }
             break;
         }

commit d531b412df6bcf16239a3dacc3fc1520349f6f89

Fix: memory leak introduced by 3abd2066f94cc50f5c8319e5af00a6d3b998c965

src/mympd_api/scripts.c
@@ -757,6 +757,7 @@ static void free_t_script_thread_arg(struct t_script_thread_arg *script_thread_a
     FREE_SDS(script_thread_arg->script_content);
     FREE_SDS(script_thread_arg->partition);
     list_free(script_thread_arg->arguments);
+    FREE_PTR(script_thread_arg);
 }
 
 /**