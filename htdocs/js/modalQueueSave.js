2fe7968cb1bc01ccd412b5b98ad2c0e5a4511ab3
jcorporation <mail@jcgames.de>
2023-08-17 22:34:36 +0200

    Feat: improve "save queue" modal #1083

--- htdocs/js/modalQueueSave.js
+++ htdocs/js/modalQueueSave.js
@@ -11,19 +11,17 @@
  */
 function initModalQueueSave() {
     document.getElementById('modalQueueSave').addEventListener('shown.bs.modal', function() {
-        const plName = document.getElementById('saveQueueName');
-        setFocus(plName);
-        plName.value = '';
-        toggleBtnGroupValueId('btnQueueSaveMode', 'create');
-        toggleSaveQueueMode(document.getElementById('btnQueueSaveMode').firstElementChild);
-        document.getElementById('saveQueueNameSelect').value = '';
-        document.getElementById('saveQueueNameSelect').filterInput.value = '';
-        filterPlaylistsSelect(1, 'saveQueueNameSelect', '', '');
+        toggleBtnGroupValueId('modalQueueSaveModeInput', 'create');
+        toggleSaveQueueMode(document.getElementById('modalQueueSaveModeInput').firstElementChild);
+        document.getElementById('modalQueueSavePlistInput').setValue('', '');
+        setDataId('modalQueueSavePlistInput', 'value', '');
+        document.getElementById('modalQueueSavePlistInput').filterInput.value = '';
+        filterPlaylistsSelect(1, 'modalQueueSavePlistInput', '', '');
         cleanupModalId('modalQueueSave');
     });
 
-    setDataId('saveQueueNameSelect', 'cb-filter', 'filterPlaylistsSelect');
-    setDataId('saveQueueNameSelect', 'cb-filter-options', [1, 'saveQueueNameSelect']);
+    setDataId('modalQueueSavePlistInput', 'cb-filter', 'filterPlaylistsSelect');
+    setDataId('modalQueueSavePlistInput', 'cb-filter-options', [1, 'modalQueueSavePlistInput']);
 }
 
 /**
@@ -35,59 +33,31 @@ function initModalQueueSave() {
 function toggleSaveQueueMode(target) {
     toggleBtnGroup(target);
     const value = getData(target, 'value');
+    const input = document.getElementById('modalQueueSavePlistInput');
+    input.setValue('', '');
     if (value === 'create') {
-        elShowId('rowSaveQueueName');
-        elHideId('rowSaveQueueNameSelect');
+        input.removeAttribute('readonly');
+        input.setAttribute('placeholder', tn('New playlist name'));
+        elHide(input.nextElementSibling);
     }
     else {
-        elHideId('rowSaveQueueName');
-        elShowId('rowSaveQueueNameSelect');
+        input.setAttribute('readonly', 'readonly');
+        input.setAttribute('placeholder', tn('Choose a playlist'));
+        elShow(input.nextElementSibling);
     }
 }
 
 /**
  * Saves the queue as a playlist
+ * @param {Element} target triggering element
  * @returns {void}
  */
 //eslint-disable-next-line no-unused-vars
-function saveQueue() {
+function saveQueue(target) {
     cleanupModalId('modalQueueSave');
-    const plNameEl = document.getElementById('saveQueueName');
-    let name = plNameEl.value;
-    let saveMode = 'create';
-    let formOK = true;
-    if (features.featAdvqueue === true) {
-        //support queue save modes (since MPD 0.24)
-        saveMode = getBtnGroupValueId('btnQueueSaveMode');
-        if (saveMode !== 'create') {
-            //append or replace existing playlist
-            name = getDataId('saveQueueNameSelect', 'value');
-        }
-        else {
-            formOK = validatePlistEl(plNameEl);
-        }
-    }
-    else {
-        formOK = validatePlistEl(plNameEl);
-    }
-    if (formOK === true) {
-        sendAPI("MYMPD_API_QUEUE_SAVE", {
-            "plist": name,
-            "mode": saveMode
-        }, saveQueueCheckError, true);
-    }
-}
-
-/**
- * Handler for the MYMPD_API_QUEUE_SAVE jsonrpc response
- * @param {object} obj jsonrpc response
- * @returns {void}
- */
-function saveQueueCheckError(obj) {
-    if (obj.error) {
-        showModalAlert(obj);
-    }
-    else {
-        uiElements.modalQueueSave.hide();
-    }
+    btnWaiting(target, true);
+    sendAPI("MYMPD_API_QUEUE_SAVE", {
+        "plist": getDataId('modalQueueSavePlistInput', 'value'),
+        "mode": getBtnGroupValueId('modalQueueSaveModeInput')
+    }, modalClose, true);
 }
