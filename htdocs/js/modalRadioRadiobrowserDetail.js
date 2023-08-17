5049538bf774b7bfdbbb9a392ca02999c6c5909d
jcorporation <mail@jcgames.de>
2023-08-18 00:11:46 +0200

    Upd: split into separate files for modals

---
+++ htdocs/js/modalRadioRadiobrowserDetail.js
@@ -0,0 +1,83 @@
+"use strict";
+// SPDX-License-Identifier: GPL-3.0-or-later
+// myMPD (c) 2018-2023 Juergen Mang <mail@jcgames.de>
+// https://github.com/jcorporation/mympd
+
+/** @module modalRadioRadiobrowserDetail_js */
+
+/**
+ * Shows the details of a radiobrowser station
+ * @param {string} uuid station uuid
+ * @returns {void}
+ */
+//eslint-disable-next-line no-unused-vars
+function showRadiobrowserDetails(uuid) {
+    sendAPI("MYMPD_API_CLOUD_RADIOBROWSER_STATION_DETAIL", {
+        "uuid": uuid
+    }, parseRadiobrowserDetails, true);
+    uiElements.modalRadiobrowserDetails.show();
+    elReplaceChildId('modalRadiobrowserDetailsList',
+        elCreateNode('tr', {}, 
+            elCreateTextTn('td', {"colspan": 2}, 'Loading...')
+        )
+    );
+    countClickRadiobrowser(uuid);
+}
+
+/**
+ * Parses the MYMPD_API_CLOUD_RADIOBROWSER_STATION_DETAIL jsonrpc response
+ * @param {object} obj jsonrpc response
+ * @returns {void}
+ */
+function parseRadiobrowserDetails(obj) {
+    const tbody = document.getElementById('modalRadiobrowserDetailsList');
+    if (checkResult(obj, tbody) === false) {
+        return;
+    }
+    elClearId('modalRadiobrowserDetailsList');
+    const result = obj.result.data[0];
+    if (result.favicon !== '') {
+        document.getElementById('RadiobrowserDetailsImage').style.backgroundImage = getCssImageUri(result.favicon);
+    }
+    else {
+        document.getElementById('RadiobrowserDetailsImage').style.backgroundImage =
+            'url("' + subdir + '/assets/coverimage-notavailable")';
+    }
+    document.getElementById('RadiobrowserDetailsTitle').textContent = result.name;
+    //map fields to webradiodb fields
+    setDataId('RadiobrowserDetailsTitle', 'webradio', {
+        "Name": result.name,
+        "StreamUri": result.url_resolved,
+        "Genre": result.tags,
+        "Homepage": result.homepage,
+        "Country": result.country,
+        "Language": result.language,
+        "Codec": result.codec,
+        "Bitrate": result.bitrate,
+        "Description": "",
+        "Image": result.favicon
+    });
+    //friendly names for fields
+    const showFields = {
+        'url_resolved': 'StreamUri',
+        'homepage': 'Homepage',
+        'tags': 'Tags',
+        'country': 'Country',
+        'language': 'Language',
+        'codec': 'Codec',
+        'bitrate': 'Bitrate',
+        'votes': 'Votes',
+        'lastchangetime': 'Last change time',
+        'lastcheckok': 'State',
+        'clickcount': 'Click count'
+    };
+    for (const field in showFields) {
+        const value = printValue(field, result[field]);
+        tbody.appendChild(
+            elCreateNodes('tr', {}, [
+                elCreateTextTn('th', {}, showFields[field]),
+                elCreateNode('td', {}, value)
+            ])
+        );
+    }
+}
---
+++ htdocs/js/modalRadioWebradiodbDetail.js
@@ -0,0 +1,83 @@
+"use strict";
+// SPDX-License-Identifier: GPL-3.0-or-later
+// myMPD (c) 2018-2023 Juergen Mang <mail@jcgames.de>
+// https://github.com/jcorporation/mympd
+
+/** @module modalRadioWebradiodbDetail_js */
+
+/**
+ * Shows the details of a webradioDB entry
+ * @param {string} uri webradio uri
+ * @returns {void}
+ */
+//eslint-disable-next-line no-unused-vars
+function showWebradiodbDetails(uri) {
+    //reuse the radiobrowser modal
+    const tbody = document.getElementById('modalRadiobrowserDetailsList');
+    elClearId('modalRadiobrowserDetailsList');
+    const m3u = isStreamUri(uri)
+        ? streamUriToName(uri) + '.m3u'
+        : uri;
+    const result = webradioDb.webradios[m3u];
+    if (result.Image !== '') {
+        document.getElementById('RadiobrowserDetailsImage').style.backgroundImage = getCssImageUri(webradioDbPicsUri + result.Image);
+    }
+    else {
+        document.getElementById('RadiobrowserDetailsImage').style.backgroundImage =
+            'url("' + subdir + '/assets/coverimage-notavailable")';
+    }
+    document.getElementById('RadiobrowserDetailsTitle').textContent = result.Name;
+    setDataId('RadiobrowserDetailsTitle', 'webradio', result);
+    const showFields = [
+        'StreamUri',
+        'Homepage',
+        'Genre',
+        'Country',
+        'Language',
+        'Codec',
+        'Bitrate',
+        'Description'
+    ];
+    for (const field of showFields) {
+        const value = printValue(field, result[field]);
+        tbody.appendChild(
+            elCreateNodes('tr', {}, [
+                elCreateTextTn('th', {}, field),
+                elCreateNode('td', {}, value)
+            ])
+        );
+    }
+    const alternateStreams = Object.keys(result.alternativeStreams);
+    if (alternateStreams.length > 0) {
+        const td = elCreateEmpty('td', {});
+        for (const name of alternateStreams) {
+            const p = elCreateTextTn('p', {"class": ["pb-0"]}, 'Webradioformat',
+                {"codec": result.alternativeStreams[name].Codec, "bitrate": result.alternativeStreams[name].Bitrate});
+            const btn = elCreateText('button', {"class": ["btn", "btn-sm", "btn-secondary", "mi", "mi-sm", "ms-2"]}, 'favorite');
+            p.appendChild(btn);
+            td.appendChild(p);
+            btn.addEventListener('click', function(event) {
+                event.preventDefault();
+                showEditRadioFavorite({
+                    "Name": result.Name,
+                    "StreamUri": result.alternativeStreams[name].StreamUri,
+                    "Genre": result.Genre,
+                    "Homepage": result.Homepage,
+                    "Country": result.Country,
+                    "Language": result.Language,
+                    "Codec": result.alternativeStreams[name].Codec,
+                    "Bitrate": result.alternativeStreams[name].Bitrate,
+                    "Description": result.Description,
+                    "Image": result.Image
+                });
+            }, false);
+        }
+        tbody.appendChild(
+            elCreateNodes('tr', {}, [
+                elCreateTextTn('th', {}, 'Alternative streams'),
+                td
+            ])
+        );
+    }
+    uiElements.modalRadiobrowserDetails.show();
+}
