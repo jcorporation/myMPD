b683468fbb8399dd788b7efc834458b4bd6b7744
jcorporation <mail@jcgames.de>
2023-08-17 17:09:59 +0200

    Feat: improve "Move to playlist"-Dialog #1083

--- htdocs/js/modalPlaylistMoveTo.js
+++ htdocs/js/modalPlaylistMoveTo.js
@@ -12,47 +12,32 @@
  * @returns {void}
  */
 function showMoveToPlaylist(srcPlist, positions) {
-    const modal = document.getElementById('modalMoveToPlaylist');
+    const modal = document.getElementById('modalPlaylistMoveTo');
     cleanupModal(modal);
     setData(modal, 'srcPlist', srcPlist);
     setData(modal, 'positions', positions);
-    filterPlaylistsSelect(1, 'moveToPlaylistPlaylist', '', '');
-    uiElements.modalMoveToPlaylist.show();
+    filterPlaylistsSelect(1, 'modalPlaylistMoveToDstPlist', '', '');
+    populateEntities('modalPlaylistMoveToPositions', positions);
+    uiElements.modalPlaylistMoveTo.show();
 }
 
 /**
  * Adds the selected elements from the "move to playlist" modal to the playlist
+ * @param {Element} target triggering element
  * @returns {void}
  */
 //eslint-disable-next-line no-unused-vars
-function moveToPlaylist() {
-    const modal = document.getElementById('modalMoveToPlaylist');
+function moveToPlaylist(target) {
+    const modal = document.getElementById('modalPlaylistMoveTo');
     cleanupModal(modal);
     const srcPlist = getData(modal, 'srcPlist');
     const positions = getData(modal, 'positions');
-    const mode = getRadioBoxValueId('moveToPlaylistPos');
-    const plistEl = document.getElementById('moveToPlaylistPlaylist');
-    if (validatePlistEl(plistEl) === false) {
-        return;
-    }
+    const mode = getRadioBoxValueId('modalPlaylistMoveToPos');
+    btnWaiting(target, true);
     sendAPI("MYMPD_API_PLAYLIST_CONTENT_MOVE_TO_PLAYLIST", {
         "srcPlist": srcPlist,
-        "dstPlist": plistEl.value,
+        "dstPlist": document.getElementById('modalPlaylistMoveToDstPlist').value,
         "positions": positions,
         "mode": Number(mode)
-    }, moveToPlaylistClose, true);
-}
-
-/**
- * Handles the response of "move to playlist" modal
- * @param {object} obj jsonrpc response
- * @returns {void}
- */
-function moveToPlaylistClose(obj) {
-    if (obj.error) {
-        showModalAlert(obj);
-    }
-    else {
-        uiElements.modalMoveToPlaylist.hide();
-    }
+    }, modalClose, true);
 }
