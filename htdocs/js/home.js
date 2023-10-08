ff1823bf32cb74b4b37bfb757aabb690c2c37053
jcorporation <mail@jcgames.de>
2023-10-08 17:58:54 +0200

    Fix: clear search field

--- htdocs/js/home.js
+++ htdocs/js/home.js
@@ -231,6 +231,7 @@ function homeIconGoto(type, uri) {
             gotoFilesystem(uri[0], type);
             break;
         case 'search':
+            elGetById('SearchSearchStr').value = '';
             appGoto('Search', undefined, undefined, 0, undefined, 'any', {'tag': 'Title', 'desc': false}, '', uri[0]);
             break;
         case 'album':
