1ffd5c26980889c92a0a8f8ba68d96eda94627af
jcorporation <mail@jcgames.de>
2023-10-10 23:26:44 +0200

    Upd: add mpack config

---
+++ dist/mpack/mpack-config.h
@@ -0,0 +1,22 @@
+/*
+ SPDX-License-Identifier: GPL-3.0-or-later
+ myMPD (c) 2018-2023 Juergen Mang <mail@jcgames.de>
+ https://github.com/jcorporation/mympd
+*/
+
+#define MPACK_READER  0
+#define MPACK_EXPECT  0
+#define MPACK_DOUBLE  0
+#define MPACK_FLOAT   0
+
+#include "compile_time.h"
+#include "src/lib/mem.h"
+
+#define MPACK_MALLOC malloc_assert
+#define MPACK_FREE free
+
+#ifdef MYMPD_DEBUG
+    #define MPACK_DEBUG 1
+#else
+    #define MPACK_DEBUG 0
+#endif
