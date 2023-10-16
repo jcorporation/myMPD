b287ee2012f1b9168b1dc62e78914f0886c62f42
jcorporation <mail@jcgames.de>
2023-10-16 22:05:51 +0200

    Upd: add sticker related debug messages

--- src/mpd_client/jukebox.c
+++ src/mpd_client/jukebox.c
@@ -639,8 +639,10 @@ static long fill_jukebox_queue_songs(struct t_partition_state *partition_state, 
     rax *stickers_last_played = NULL;
     rax *stickers_like = NULL;
     if (partition_state->mpd_state->feat_stickers == true) {
+        MYMPD_LOG_DEBUG(partition_state->name, "Fetching lastPlayed stickers");
         stickers_last_played = stickerdb_find_stickers_by_name(partition_state->mympd_state->stickerdb, "lastPlayed");
         if (partition_state->jukebox_ignore_hated == true) {
+            MYMPD_LOG_DEBUG(partition_state->name, "Fetching like stickers");
             stickers_like = stickerdb_find_stickers_by_name(partition_state->mympd_state->stickerdb, "like");
         }
     }
