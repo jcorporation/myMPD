ce540c302f351255480fe8ab94585ca12f5b0974
jcorporation <mail@jcgames.de>
2023-10-29 18:16:32 +0100

    Upd: set version to 14.0.0

--- htdocs/js/version.js
+++ htdocs/js/version.js
@@ -1,2 +1,2 @@
-const myMPDversion = '13.0.0';
-const myMPDbuild = '25a89b1c244cf7fed62f3ba0e79e0ee3946828e9';
+const myMPDversion = '14.0.0';
+const myMPDbuild = '666d45a84a5814b55c0a56c3ac34f021a9e744a8';
