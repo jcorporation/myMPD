3008ce48cc2841ea3d7ab8f10d2132fc2824d4bd
jcorporation <mail@jcgames.de>
2023-12-12 23:03:56 +0100

    Upd: utf8.h dependency

--- dist/utf8/utf8.h
+++ dist/utf8/utf8.h
@@ -61,6 +61,10 @@ typedef int32_t utf8_int32_t;
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wold-style-cast"
 #pragma clang diagnostic ignored "-Wcast-qual"
+
+#if __has_warning("-Wunsafe-buffer-usage")
+#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
+#endif
 #endif
 
 #ifdef __cplusplus
@@ -784,7 +788,7 @@ utf8_constexpr14_impl utf8_int8_t *utf8rchr(const utf8_int8_t *src, int chr) {
   while ('\0' != *src) {
     size_t offset = 0;
 
-    while (src[offset] == c[offset]) {
+    while ((src[offset] == c[offset]) && ('\0' != src[offset])) {
       offset++;
     }
 
@@ -792,6 +796,10 @@ utf8_constexpr14_impl utf8_int8_t *utf8rchr(const utf8_int8_t *src, int chr) {
       /* we found a matching utf8 code point */
       match = (utf8_int8_t *)src;
       src += offset;
+
+      if ('\0' == *src) {
+        break;
+      }
     } else {
       src += offset;
 
