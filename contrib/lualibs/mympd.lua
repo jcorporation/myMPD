3844aa02351e1a8c3564c450cdb8566edf663203
jcorporation <mail@jcgames.de>
2024-02-09 00:08:06 +0100

    Fix: typo

--- contrib/lualibs/mympd.lua
+++ contrib/lualibs/mympd.lua
@@ -71,28 +71,28 @@ mympd.mygpiod_socket = "/run/mygpiod/socket"
 -- Blinks a GPIO with given timeout and interval
 --
 function mympd.gpio_blink(gpio, timeout_ms, interval_ms)
-  return mygpio_gpio_blink(mymypd.mygpiod_socket, gpio, timeout_ms, interval_ms)
+  return mygpio_gpio_blink(mympd.mygpiod_socket, gpio, timeout_ms, interval_ms)
 end
 
 --
 -- Returns the active state of a GPIO
 --
 function mympd.gpio_get(gpio)
-  return mygpio_gpio_get(mymypd.mygpiod_socket, gpio)
+  return mygpio_gpio_get(mympd.mygpiod_socket, gpio)
 end
 
 --
 -- Sets the active value of a GPIO
 --
 function mympd.gpio_set(gpio, value)
-  return mygpio_gpio_set(mymypd.mygpiod_socket, gpio, value)
+  return mygpio_gpio_set(mympd.mygpiod_socket, gpio, value)
 end
 
 --
 -- Toggles the active value of a GPIO
 --
 function mympd.gpio_toggle(gpio)
-  return mygpio_gpio_toggle(mymypd.mygpiod_socket, gpio)
+  return mygpio_gpio_toggle(mympd.mygpiod_socket, gpio)
 end
 
 return mympd
