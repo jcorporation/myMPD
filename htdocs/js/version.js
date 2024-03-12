261501025175d4f543a946af376fc2c88e9aeb07
jcorporation <mail@jcgames.de>
2024-03-12 20:20:35 +0100

    Upd: version

--- htdocs/js/version.js
+++ htdocs/js/version.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 const myMPDversion = '15.0.0';
-const myMPDbuild = 'c85972c92eb6393755ebd80286cee7ae0abfa987';
-=======
-const myMPDversion = '14.1.0';
-const myMPDbuild = '7799f4dd2531fb1b89d3a126e75fb530b0e079d4';
->>>>>>> devel
+const myMPDbuild = '4dc84fbce6af2a6bd5f28aee7779ba927f7a22a6';
