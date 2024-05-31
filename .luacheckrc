b0a48f1e3efd184830d52ebd3390fca945256ada
jcorporation <mail@jcgames.de>
2024-06-01 00:55:56 +0200

    Fix: lua linter warnings

--- .luacheckrc
+++ .luacheckrc
@@ -12,6 +12,7 @@ read_globals = {
     "mympd_api",
     "mympd_http_client",
     "mympd_http_download",
+    "mympd_http_serve_file",
     "mympd_util_imagescache_write",
     "mympd_util_notify",
     "mympd_util_log",
--- contrib/lualibs/mympd/60-caches.lua
+++ contrib/lualibs/mympd/60-caches.lua
@@ -15,27 +15,27 @@ function mympd.tmp_file()
     end
     return mympd_env.cachedir_misc .. "/" .. table.concat(ret) .. ".tmp"
   end
-  
-  --- Write a file for the cover cache
-  -- @param src File to rename
-  -- @param uri URI to create the cover cache file for
-  -- @return written name
-  function mympd.covercache_write(src, uri)
-    return mympd_util_imagescache_write("cover", src, uri)
-  end
-  
-  --- Write a file for the thumbs cache
-  -- @param src File to rename
-  -- @param tagvalue Tag value to create the thumbs cache file for
-  -- @return written name
-  function mympd.thumbscache_write(src, tagvalue)
-    return mympd_util_imagescache_write("thumbs", src, tagvalue)
-  end
-  
-  --- Write a string to a file in the lyrics cache
-  -- @param str String to save (it must be a valid lyrics json string)
-  -- @param uri URI to create the lyrics cache file for
-  -- @return written name
-  function mympd.lyricscache_write(str, uri)
-    return mympd_util_lyricscache_write(str, uri)
-  end
+
+--- Write a file for the cover cache
+-- @param src File to rename
+-- @param uri URI to create the cover cache file for
+-- @return written name
+function mympd.covercache_write(src, uri)
+  return mympd_util_imagescache_write("cover", src, uri)
+end
+
+--- Write a file for the thumbs cache
+-- @param src File to rename
+-- @param tagvalue Tag value to create the thumbs cache file for
+-- @return written name
+function mympd.thumbscache_write(src, tagvalue)
+  return mympd_util_imagescache_write("thumbs", src, tagvalue)
+end
+
+--- Write a string to a file in the lyrics cache
+-- @param str String to save (it must be a valid lyrics json string)
+-- @param uri URI to create the lyrics cache file for
+-- @return written name
+function mympd.lyricscache_write(str, uri)
+  return mympd_util_lyricscache_write(str, uri)
+end
