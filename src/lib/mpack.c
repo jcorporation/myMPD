b4abf208763ee7b7ef4aa710225b632dad4dbcc4
jcorporation <mail@jcgames.de>
2024-06-24 20:42:44 +0200

    Feat: new mpack to sds functions

--- src/lib/mpack.c
+++ src/lib/mpack.c
@@ -18,3 +18,25 @@ void log_mpack_write_error(mpack_writer_t *writer, mpack_error_t error) {
     (void) writer;
     MYMPD_LOG_ERROR("default", "mpack error: %s", mpack_error_to_string(error));
 }
+
+/**
+ * Creates a sds string from a mpack str
+ * @param node node to get the key from
+ * @param key key to get
+ * @return newly allocated sds string
+ */
+sds mpackstr_sds(mpack_node_t node, const char *key) {
+    mpack_node_t data = mpack_node_map_cstr(node, key);
+    return sdsnewlen(mpack_node_str(data), mpack_node_data_len(data));
+}
+
+/**
+ * Appends a mpack str to a sds string
+ * @param node node to get the key from
+ * @param key key to get
+ * @return newly allocated sds string
+ */
+sds mpackstr_sdscat(sds s, mpack_node_t node, const char *key) {
+    mpack_node_t data = mpack_node_map_cstr(node, key);
+    return sdscatlen(s, mpack_node_str(data), mpack_node_data_len(data));
+}
--- src/lib/mpack.h
+++ src/lib/mpack.h
@@ -8,8 +8,12 @@
 #define MYMPD_MPACK_H
 
 #include "dist/mpack/mpack.h"
+#include "dist/sds/sds.h"
 
 void log_mpack_node_error(mpack_tree_t *tree, mpack_error_t error);
 void log_mpack_write_error(mpack_writer_t *writer, mpack_error_t error);
+
+sds mpackstr_sds(mpack_node_t node, const char *key);
+sds mpackstr_sdscat(sds s, mpack_node_t node, const char *key);
 
 #endif
