cc24760a1f9938eb699786018bb003ba2556f93b
jcorporation <mail@jcgames.de>
2024-09-17 19:02:11 +0200

    Fix: do not set tag

--- htdocs/js/browse.js
+++ htdocs/js/browse.js
@@ -145,7 +145,7 @@ function gotoAlbumList(tag, value) {
         expression += '(' + tag + ' == \'' + escapeMPD(value[i]) + '\')';
     }
     expression += ')';
-    appGoto('Browse', 'Database', 'AlbumList', 0, undefined, tag, {'tag': settings.webuiSettings.browseDatabaseAlbumListSort, 'desc': false}, 'Album', expression);
+    appGoto('Browse', 'Database', 'AlbumList', 0, undefined, 'any', {'tag': settings.webuiSettings.browseDatabaseAlbumListSort, 'desc': false}, 'Album', expression);
 }
 
 /**
