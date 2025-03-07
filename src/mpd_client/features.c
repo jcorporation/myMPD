b0a11456ec9af2b9b1ce2c0023764f4fcd181adf
jcorporation <mail@jcgames.de>
2025-03-07 20:06:23 +0100

    Upd: improve logging

--- src/mpd_client/features.c
+++ src/mpd_client/features.c
@@ -397,16 +397,16 @@ static sds set_directory(const char *desc, sds directory, sds value) {
     }
     strip_slash(value);
     if (sdslen(value) > 0 &&
-        testdir("Directory", value, false, false) != DIR_EXISTS)
+        testdir(desc, value, false, true) != DIR_EXISTS)
     {
-        MYMPD_LOG_WARN(NULL, "Directory %s not accessible", value);
+        MYMPD_LOG_WARN(NULL, "MPD %s directory %s not accessible", desc, value);
         sdsclear(value);
     }
     if (sdslen(value) == 0) {
         MYMPD_LOG_WARN(NULL, "MPD %s directory is not set", desc);
     }
     else {
-        MYMPD_LOG_INFO(NULL, "MPD %s directory is \"%s\"", desc, value);
+        MYMPD_LOG_NOTICE(NULL, "MPD %s directory is \"%s\"", desc, value);
     }
     return value;
 }
