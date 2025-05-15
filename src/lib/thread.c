45f4ac1dcc6481fbfb0962b917b4127e3b79bf31
jcorporation <mail@jcgames.de>
2025-05-16 00:17:58 +0200

    Fix: Move include

--- src/lib/thread.c
+++ src/lib/thread.c
@@ -11,8 +11,6 @@
 #include "compile_time.h"
 #include "src/lib/thread.h"
 
-#include "src/lib/log.h"
-
 _Atomic int mympd_worker_threads;
 #ifdef MYMPD_ENABLE_LUA
     _Atomic int script_worker_threads;
@@ -31,6 +29,8 @@ void set_threadname(const char *threadname) {
 }
 
 #else
+
+#include "src/lib/log.h"
 
 #include <string.h>
 
