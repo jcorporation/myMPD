5266b4a3369d6b51ea61ba0f3d3e1fe84b57d5f8
jcorporation <mail@jcgames.de>
2025-10-10 19:33:04 +0200

    Fix: includes

--- src/lib/album.c
+++ src/lib/album.c
@@ -11,6 +11,7 @@
 #include "compile_time.h"
 #include "src/lib/album.h"
 
+#include "src/lib/cache/cache_rax_album.h"
 #include "src/lib/convert.h"
 #include "src/lib/json/json_print.h"
 #include "src/lib/mem.h"
