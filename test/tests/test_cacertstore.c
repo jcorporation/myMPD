6b2c28e37046d0deb42f85dc4825b0f95bd6eb5a
jcorporation <mail@jcgames.de>
2025-10-10 20:20:27 +0200

    Upd: Add test for cacertstore

---
+++ test/tests/test_cacertstore.c
@@ -0,0 +1,16 @@
+/*
+ SPDX-License-Identifier: GPL-3.0-or-later
+ myMPD (c) 2018-2025 Juergen Mang <mail@jcgames.de>
+ https://github.com/jcorporation/mympd
+*/
+
+#include "compile_time.h"
+#include "utility.h"
+
+#include "dist/utest/utest.h"
+#include "src/lib/cacertstore.h"
+
+UTEST(cacertstore, test_find_ca_cert_store) {
+    const char *cabundle = find_ca_cert_store(false);
+    ASSERT_TRUE(cabundle != NULL);
+}
