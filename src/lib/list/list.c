46a8cb934e0f0ce2386ffeb7adf13f838cab354e
jcorporation <mail@jcgames.de>
2026-02-02 19:54:04 +0100

    Fix: merge

--- src/lib/list/list.c
+++ src/lib/list/list.c
@@ -14,9 +14,7 @@
 #include "src/lib/filehandler.h"
 #include "src/lib/log.h"
 #include "src/lib/mem.h"
-#include "src/lib/random.h"
 #include "src/lib/sds/sds_extras.h"
-#include <src/lib/utf8_wrapper.h>
 
 #include <string.h>
 
